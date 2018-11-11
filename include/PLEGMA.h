c8bc2742e1ce70372697f830252565273a76be7f
sbacchio <s.bacchio@gmail.com>
2018-11-11 17:58:36 +0200

    Moving PLEGMA_params in global

--- include/PLEGMA.h
+++ include/PLEGMA.h
@@ -9,40 +9,7 @@
 #define _PLEGMA__H
 
 namespace plegma {
-
-  typedef struct {
-    int nsmearAPE;
-    int nsmearGauss;
-    double alphaAPE;
-    double alphaGauss;
-    int lL[N_DIMS];
-    int procs[N_DIMS];
-    int Nsources;
-    int sourcePosition[MAX_NSOURCES][N_DIMS];
-    QudaPrecision Precision;
-    int Q_sq;
-    int Ntsink;
-    int Nproj[MAX_TSINK];
-    int traj;
-    bool check_files;
-    char *thrp_type[3];
-    char *thrp_proj_type[5];
-    char *baryon_type[10];
-    char *meson_type[10];
-    int tsinkSource[MAX_TSINK];
-    int proj_list[MAX_TSINK][MAX_PROJS];
-    int run3pt_src[MAX_NSOURCES];
-    FILE_WRITE_FORMAT CorrFileFormat;
-    SOURCE_T source_type;
-    CORR_SPACE CorrSpace;
-    bool HighMomForm;
-    bool isEven;
-    double kappa;
-    double mu;
-    double csw;
-    double inv_tol;
-  } PLEGMA_params;
-
+  
   void initialize(PLEGMA_params *params);
 
   void print_status();
--- include/PLEGMA_global.h
+++ include/PLEGMA_global.h
@@ -94,6 +94,39 @@ namespace plegma {
   enum THRP_TYPE{THRP_LOCAL2,THRP_NOETHER2,THRP_ONED2};
 
   enum APEDIM{D3,D4};
+
+  typedef struct {
+    int nsmearAPE;
+    int nsmearGauss;
+    double alphaAPE;
+    double alphaGauss;
+    int lL[N_DIMS];
+    int procs[N_DIMS];
+    int Nsources;
+    int sourcePosition[MAX_NSOURCES][N_DIMS];
+    QudaPrecision Precision;
+    int Q_sq;
+    int Ntsink;
+    int Nproj[MAX_TSINK];
+    int traj;
+    bool check_files;
+    char *thrp_type[3];
+    char *thrp_proj_type[5];
+    char *baryon_type[10];
+    char *meson_type[10];
+    int tsinkSource[MAX_TSINK];
+    int proj_list[MAX_TSINK][MAX_PROJS];
+    int run3pt_src[MAX_NSOURCES];
+    FILE_WRITE_FORMAT CorrFileFormat;
+    SOURCE_T source_type;
+    CORR_SPACE CorrSpace;
+    bool HighMomForm;
+    bool isEven;
+    double kappa;
+    double mu;
+    double csw;
+    double inv_tol;
+  } PLEGMA_params;
 }
 
 #endif
