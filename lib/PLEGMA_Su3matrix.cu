916f29c85cf29bf4158ef62ea6486e5ae911477a
Simone Bacchio <s.bacchio@gmail.com>
2018-11-13 19:34:20 +0200

    Adding absorbDir in device and host

--- lib/PLEGMA_Su3matrix.cu
+++ lib/PLEGMA_Su3matrix.cu
@@ -11,10 +11,16 @@ PLEGMA_Su3matrix<Float>::PLEGMA_Su3matrix(ALLOCATION_FLAG alloc_flag):
   PLEGMA_Field<Float>(alloc_flag, SU3FIELD){ ; }
 
 template<typename Float>
-void PLEGMA_Su3matrix<Float>::absorbDir(PLEGMA_Gauge<Float> &u,int dir){
-  cudaMemcpy(PLEGMA_Field<Float>::d_elem, u.D_elem()+dir*(PLEGMA_Field<Float>::field_length)*(PLEGMA_Field<Float>::total_length)*2,
-  	     PLEGMA_Field<Float>::bytes_total_length, cudaMemcpyDeviceToDevice);
+void PLEGMA_Su3matrix<Float>::absorbDir_device(PLEGMA_Gauge<Float> &u,int dir){
+  cudaMemcpy(this->d_elem, u.D_elem()+dir*(this->field_length)*(this->total_length)*2,
+  	     this->bytes_total_length, cudaMemcpyDeviceToDevice);
   checkCudaError();
+}
+
+template<typename Float>
+void PLEGMA_Su3matrix<Float>::absorbDir_host(PLEGMA_Gauge<Float> &u,int dir){
+  memcpy(this->h_elem, u.H_elem()+dir*(this->field_length)*(this->total_length)*2,
+	 this->bytes_total_length);
 }
 
 template class PLEGMA_Su3matrix<float>;
