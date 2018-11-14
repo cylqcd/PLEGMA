9372c50fed53fa231e747080b5af7df29953a053
Srijit Paul <srijitpaul@gmail.com>
2018-11-14 11:40:14 +0200

    Added google test

--- plegma/Calc_2pt.cpp
+++ plegma/Calc_2pt.cpp
@@ -82,7 +82,7 @@ int main(int argc, char **argv)
 	       params.sourcePosition[isource][3]);
 
     for(int isc = 0 ; isc < 12 ; isc++){
-      vectorAuxD.pointSource(params.sourcePosition[isource], isc/3, isc%3);
+      vectorAuxD.pointSource(params.sourcePosition[isource], isc/3, isc%3, HOST);
       vectorAuxD.loadVector();
       vectorIn.gaussianSmearing(vectorAuxD,smearedGauge);
       
