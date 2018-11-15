ccda2b47c4a2665d5bdd44ba2edff66a9b4f7a0a
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2018-11-15 20:52:21 +0200

    Fix bug for if c_threads > than <

--- lib/kernels/PLEGMA_shifts.cuh
+++ lib/kernels/PLEGMA_shifts.cuh
@@ -4,12 +4,14 @@ using namespace plegma;
 template<typename Float>
 static __global__ void shifts_kernel(Float *in, Float *out, int length_field, int dirOr) {
   int sid = blockIdx.x*blockDim.x + threadIdx.x;
-  if(sid < c_threads) return;
+  if(sid >= c_threads) return;
   generic2<Float> R(in);
   Float2<Float> *out2 = (Float2<Float> *) out;
   #pragma unroll
-  for(int i = 0 ; i < length_field ; i++)
-    out2[i*c_stride + sid] = (dirOr<4)?R.getMinus(i,length_field,dirOr%4,sid):R.getPlus(i,length_field,dirOr%4,sid);
+  for(int i = 0 ; i < length_field ; i++){
+    Float2<Float> tmp = (dirOr<4)?R.getMinus(i,length_field,dirOr%4,sid):R.getPlus(i,length_field,dirOr%4,sid);
+    out2[i*c_stride + sid] = tmp;
+  }
 }
 
 template<typename Float>
