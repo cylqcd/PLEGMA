bfd9c4e92aa1330bf55be2019cb36f0b6bf47017
Srijit Paul <srijitpaul@gmail.com>
2018-11-15 15:07:55 +0200

    Added 1-D to 4-D inflator

--- lib/kernels/PLEGMA_kernel_utils.cuh
+++ lib/kernels/PLEGMA_kernel_utils.cuh
@@ -18,78 +18,88 @@
 
 namespace plegma {
   template<typename FloatA, typename FloatB, typename FloatC>
-  __inline__ __device__ void mul_G_G(Float2<FloatA> a[N_COLS][N_COLS], Float2<FloatB> b[N_COLS][N_COLS], Float2<FloatC> c[N_COLS][N_COLS]){
-  #pragma unroll
-  for(int i=0; i<N_COLS; i++)
-    #pragma unroll
-    for(int j=0; j<N_COLS; j++) {
-      a[i][j] = 0.;
-      #pragma unroll
-      for(int k=0; k<N_COLS; k++) {
-        a[i][j] = a[i][j] + b[i][k]*c[k][j];
-      }
+    __inline__ __device__ void mul_G_G(Float2<FloatA> a[N_COLS][N_COLS], Float2<FloatB> b[N_COLS][N_COLS], Float2<FloatC> c[N_COLS][N_COLS]){
+#pragma unroll
+      for(int i=0; i<N_COLS; i++)
+#pragma unroll
+        for(int j=0; j<N_COLS; j++) {
+          a[i][j] = 0.;
+#pragma unroll
+          for(int k=0; k<N_COLS; k++) {
+            a[i][j] = a[i][j] + b[i][k]*c[k][j];
+          }
+        }
     }
-  }
 
   template<typename FloatA, typename FloatB, typename FloatC>
-  __inline__ __device__ void mul_Gdag_Gdag(Float2<FloatA> a[N_COLS][N_COLS], Float2<FloatB> b[N_COLS][N_COLS], Float2<FloatC> c[N_COLS][N_COLS]){
-    #pragma unroll
-    for(int i=0; i<N_COLS; i++)
-    #pragma unroll
-    for(int j=0; j<N_COLS; j++) {
-      a[i][j] = 0.;
-      #pragma unroll
-      for(int k=0; k<N_COLS; k++) {
-        Float2<FloatA> tmp = c[j][k]*b[k][i];
-        a[i][j].x += tmp.x;
-        a[i][j].y -= tmp.y;
-      }
+    __inline__ __device__ void mul_Gdag_Gdag(Float2<FloatA> a[N_COLS][N_COLS], Float2<FloatB> b[N_COLS][N_COLS], Float2<FloatC> c[N_COLS][N_COLS]){
+#pragma unroll
+      for(int i=0; i<N_COLS; i++)
+#pragma unroll
+        for(int j=0; j<N_COLS; j++) {
+          a[i][j] = 0.;
+#pragma unroll
+          for(int k=0; k<N_COLS; k++) {
+            Float2<FloatA> tmp = c[j][k]*b[k][i];
+            a[i][j].x += tmp.x;
+            a[i][j].y -= tmp.y;
+          }
+        }
     }
-  }
 
   template<typename FloatA, typename FloatB>
-  __inline__ __device__ FloatA real_trace_mul_G_G(Float2<FloatA> a[N_COLS][N_COLS], Float2<FloatB> b[N_COLS][N_COLS]){
-    FloatA tr=0.;
-    #pragma unroll
-    for(int i=0; i<N_COLS; i++){
-      #pragma unroll
-      for(int j=0; j<N_COLS; j++) {
-	tr+=(a[i][j]*b[j][i]).x;
+    __inline__ __device__ FloatA real_trace_mul_G_G(Float2<FloatA> a[N_COLS][N_COLS], Float2<FloatB> b[N_COLS][N_COLS]){
+      FloatA tr=0.;
+#pragma unroll
+      for(int i=0; i<N_COLS; i++){
+#pragma unroll
+        for(int j=0; j<N_COLS; j++) {
+          tr+=(a[i][j]*b[j][i]).x;
+        }
       }
+      return tr;
     }
-    return tr;
-  }
 
   template<typename FloatOutV, typename FloatG, typename FloatInV>
-  __inline__ __device__ void mul_G_V(Float2<FloatOutV> outV[N_SPINS][N_COLS],
-				     Float2<FloatG> G[N_COLS][N_COLS],
-				     Float2<FloatInV> inV[N_SPINS][N_COLS]){
-     #pragma unroll
-     for(int mu=0; mu<N_SPINS; mu++)
-       #pragma unroll
-       for(int j=0; j<N_COLS; j++) {
-	 outV[mu][j] = 0.;
-         #pragma unroll
-	 for(int k=0; k<N_COLS; k++) {
-	   outV[mu][j] = outV[mu][j] + G[j][k]*inV[mu][k];
-	 }
-       }
-  }
+    __inline__ __device__ void mul_G_V(Float2<FloatOutV> outV[N_SPINS][N_COLS],
+        Float2<FloatG> G[N_COLS][N_COLS],
+        Float2<FloatInV> inV[N_SPINS][N_COLS]){
+#pragma unroll
+      for(int mu=0; mu<N_SPINS; mu++)
+#pragma unroll
+        for(int j=0; j<N_COLS; j++) {
+          outV[mu][j] = 0.;
+#pragma unroll
+          for(int k=0; k<N_COLS; k++) {
+            outV[mu][j] = outV[mu][j] + G[j][k]*inV[mu][k];
+          }
+        }
+    }
 
   template<typename FloatOutV, typename FloatG, typename FloatInV>
-  __inline__ __device__ void mul_Gdag_V(Float2<FloatOutV> outV[N_SPINS][N_COLS],
-					Float2<FloatG> G[N_COLS][N_COLS],
-					Float2<FloatInV> inV[N_SPINS][N_COLS]){
-     #pragma unroll
-     for(int mu=0; mu<N_SPINS; mu++)
-       #pragma unroll
-       for(int j=0; j<N_COLS; j++) {
-	 outV[mu][j] = 0.;
-         #pragma unroll
-	 for(int k=0; k<N_COLS; k++) {
-	   outV[mu][j] = outV[mu][j] + conj(G[k][j])*inV[mu][k];
-	 }
-       }
+    __inline__ __device__ void mul_Gdag_V(Float2<FloatOutV> outV[N_SPINS][N_COLS],
+        Float2<FloatG> G[N_COLS][N_COLS],
+        Float2<FloatInV> inV[N_SPINS][N_COLS]){
+#pragma unroll
+      for(int mu=0; mu<N_SPINS; mu++)
+#pragma unroll
+        for(int j=0; j<N_COLS; j++) {
+          outV[mu][j] = 0.;
+#pragma unroll
+          for(int k=0; k<N_COLS; k++) {
+            outV[mu][j] = outV[mu][j] + conj(G[k][j])*inV[mu][k];
+          }
+        }
+    }
+
+  __inline__ void get_coords(int id, int *position){
+  
+      float temp = id/GK_localVolume;
+      for(int i = N_DIMS-1; i >=0; --i) {
+        
+        temp = temp * GK_localL[i];
+        position[i] = (int)temp % GK_localL[i];
+      }
   }
 }
 #endif
