d89d48dbf34546368cce2d06c089ed9ae1f9d24e
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2018-12-10 20:21:18 +0200

    Device functions for apply Gamma on vec or prop

--- lib/kernels/PLEGMA_kernel_utils.cuh
+++ lib/kernels/PLEGMA_kernel_utils.cuh
@@ -21,7 +21,38 @@
 using namespace plegma;
 
 namespace plegma {
+  enum LEFTRIGHT {LEFT, RIGHT};
   
+  template<LEFTRIGHT LF,typename Float>
+  __inline__ __device__ void gammaV(Float2<Float> vout[N_SPINS][N_COLS], Float2<Float>vin[N_SPINS][N_COLS], short int r){
+#pragma unroll
+    for(int nz = 0; nz < N_SPINS; nz++){
+      int mu = (LF == LEFT)? gammaInd[r][nz][0] : gammaInd[r][nz][1];
+      int nu = (LF == LEFT)? gammaInd[r][nz][1] : gammaInd[r][nz][0];
+#pragma unroll
+      for(int c1 = 0; c1 < N_COLS; c1++)
+	vout[mu][c1] = gamma[r][nz]*vin[nu][c1] ;
+    }
+  }
+
+  template<LEFTRIGHT LF,typename Float>
+  __inline__ __device__ void gammaProp(Float2<Float> pout[N_SPINS][N_SPINS][N_COLS][N_COLS],
+				       Float2<Float> pin[N_SPINS][N_SPINS][N_COLS][N_COLS], short int r){
+#pragma unroll
+    for(int nu = 0 ; nu < N_SPINS; nu++)
+#pragma unroll
+    for(int nz = 0; nz < N_SPINS; nz++){
+      int mu = (LF == LEFT)? gammaInd[r][nz][0] : gammaInd[r][nz][1];
+      int rho = (LF == LEFT)? gammaInd[r][nz][1] : gammaInd[r][nz][0];
+#pragma unroll
+      for(int c1 = 0; c1 < N_COLS; c1++)
+	for(int c2 = 0; c2 < N_COLS; c2++){
+	  Float2<Float> p = (LF == LEFT)? pin[rho][nu][c1][c2]: pin[nu][rho][c1][c2];
+	  pout[nu][mu][c1][c2] = gamma[r][nz]*p ;
+	}
+    }
+  }
+
   template<typename Float>
   __inline__ __device__ Float2<Float> det(Float2<Float> a[N_COLS][N_COLS]){
   return a[0][1]*a[1][2]*a[2][0] + a[0][2]*a[1][0]*a[2][1] +a[0][0]*a[1][1]*a[2][2]
