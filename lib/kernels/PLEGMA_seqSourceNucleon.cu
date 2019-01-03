5340f00503b30773fe09b3390698139e85ca8911
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-01-03 17:09:11 +0200

    Fix linking problem with static functions

--- lib/kernels/PLEGMA_seqSourceNucleon.cu
+++ lib/kernels/PLEGMA_seqSourceNucleon.cu
@@ -36,20 +36,24 @@ __device__ void contractNucleonSeqSource(FloatC* vec, genericTex<FloatA> prop1, 
   prop1.get(pP,N_SPINS*N_SPINS*N_COLS*N_COLS,ss);
   if(isTwoPropDiff) prop2.get(pP2, N_SPINS*N_SPINS*N_COLS*N_COLS,ss);
 
+//#pragma unroll
   for(short cc1 = 0 ; cc1 < 6 ; cc1++){
     short c1 = eps[cc1][0];
     short c2 = eps[cc1][1];
     short c3 = eps[cc1][2];
+//#pragma unroll
     for(short cc2 = 0 ; cc2 < 6 ; cc2++){
       short c1p = eps[cc2][0];
       short c2p = eps[cc2][1];
       short c3p = eps[cc2][2];
       if(c3p == c_c2)
+//#pragma unroll
 	for(short idx = 0 ; idx < 16 ; idx++){
 	  short mu = NtoN_indices[idx][0];
 	  short nu = NtoN_indices[idx][1];
 	  short ku = NtoN_indices[idx][2];
 	  short lu = NtoN_indices[idx][3];
+//#pragma unroll
 	  for(short nz = 0; nz < 8; nz++){
 	    int b = prInd[proj][nz][0];
 	    int a = prInd[proj][nz][1];
@@ -61,6 +65,7 @@ __device__ void contractNucleonSeqSource(FloatC* vec, genericTex<FloatA> prop1, 
 	      }
 	    }
 	    else
+//#pragma unroll
 	      for(short gu = 0 ; gu < 4 ; gu++){
                 if( mu == gu && b == c_nu ) spinor[gu][c3] = spinor[gu][c3] + factor * P2[nu][lu][c1][c1p] * P[a][ku][c2][c2p];
                 if( mu == gu && ku == c_nu ) spinor[gu][c3] = spinor[gu][c3] + factor * P2[nu][lu][c1][c1p] * P[a][b][c2][c2p];
@@ -69,7 +74,9 @@ __device__ void contractNucleonSeqSource(FloatC* vec, genericTex<FloatA> prop1, 
 	      }
 	  }   
 	}}}
+//#pragma unroll
   for(short mu = 0 ; mu < 4 ; mu++)
+//#pragma unroll
     for(short ic = 0 ; ic < 3 ; ic++)
       vec2[(mu*N_COLS + ic)*c_stride + timeslice*space_stride + sid] = spinor[mu][ic];
 }
@@ -110,13 +117,13 @@ static void contractNucleonSeqSource(PLEGMA_Vector<FloatC> &vec, genericTex<Floa
 }
 
 template<typename FloatC, typename FloatA>
-static void contractNucleonSeqSource(PLEGMA_Vector<FloatC> &vec, genericTex<FloatA> prop1,WHICHPROJECTOR proj, WHICHPARTICLE particle, int timeslice, int c_nu, int c_c2){
+void contractNucleonSeqSource(PLEGMA_Vector<FloatC> &vec, genericTex<FloatA> prop1,WHICHPROJECTOR proj, WHICHPARTICLE particle, int timeslice, int c_nu, int c_c2){
   genericTex<FloatA> prop2 = prop1;
   contractNucleonSeqSource(vec, prop1,prop2, proj,particle, timeslice, false, c_nu, c_c2);
 }
 
 template<typename FloatC, typename FloatA, typename FloatB>
-static void contractNucleonSeqSource(PLEGMA_Vector<FloatC> &vec, genericTex<FloatA> prop1, genericTex<FloatB> prop2,WHICHPROJECTOR proj, WHICHPARTICLE particle, int timeslice, int c_nu, int c_c2){
+void contractNucleonSeqSource(PLEGMA_Vector<FloatC> &vec, genericTex<FloatA> prop1, genericTex<FloatB> prop2,WHICHPROJECTOR proj, WHICHPARTICLE particle, int timeslice, int c_nu, int c_c2){
   contractNucleonSeqSource(vec, prop1, prop2, proj,particle, timeslice, true, c_nu, c_c2);
 }
 
