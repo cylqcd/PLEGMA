ea57b4d1e6ea484844883230bf6fe0a88cfa16d1
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-01-22 16:33:45 +0200

    Add support to multiply a plegma field with e^{+- i x p}

--- include/PLEGMA_Field.h
+++ include/PLEGMA_Field.h
@@ -85,7 +85,7 @@ namespace plegma {
     
     void shift(PLEGMA_Field &Fin, int dirOr);
     void setUnit(std::vector<int> indDiag);
-
+    void mulMomentumPhases(std::vector<int> mom, int sign=-1);
   };
 }
 #endif
