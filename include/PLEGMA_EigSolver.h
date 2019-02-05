144bf3e354e87c1928e4a14dcac016b2a930133b
Kyriakos Hadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-02-05 03:46:40 -0500

    Add polynomial operator

--- include/PLEGMA_EigSolver.h
+++ include/PLEGMA_EigSolver.h
@@ -1,4 +1,4 @@
-#include <PLEGMA.h>
+#include <PLEGMA_utils.h>
 #ifndef PLEGMA_EIGSOLVER_H
 #define PLEGMA_EIGSOLVER_H
 
@@ -9,13 +9,26 @@
 #if defined(PRIMME_EIGSOLVER)
 #include <primme.h>
 #elif defined(ARPACK_EIGSOLVER)
-
+extern "C"{
+  extern int initlog_(int*, char*, int);
+  extern int finilog_(int*);
+  extern int pznaupd_(int *comm, int *ido, char *bmat, int *n, char *which, int *nev, double *tol,
+			    std::complex<double> *resid, int *ncv, std::complex<double> *v, int *ldv, 
+			    int *iparam, int *ipntr, std::complex<double> *workd, std::complex<double> *workl, 
+			    int *lworkl, double *rwork, int *info, int bmat_size, int which_size );
+  extern int pzneupd_(int *comm, int *comp_evecs, char *howmany, int *select, std::complex<double> *evals, 
+			    std::complex<double> *v, int *ldv, std::complex<double> *sigma, std::complex<double> *workev, 
+			    char *bmat, int *n, char *which, int *nev, double *tol, std::complex<double> *resid, 
+			    int *ncv, std::complex<double> *v1, int *ldv1, int *iparam, int *ipntr, 
+			    std::complex<double> *workd, std::complex<double> *workl, int *lworkl, double *rwork, int *info,
+			    int howmany_size, int bmat_size, int which_size);
+  extern int pmcinitdebug_(int*,int*,int*,int*,int*,int*,int*,int*);
+}
 #else
 #error Neither PRIMME nor ARPACK have been defined
 #endif
 
-class  PLEGMA_EigSolverParams{
- protected:
+class  EigSolverParams{
   int NeV; // total number of eigenvalues & eigenvectors
   int NkV; // Krylov space size > NeV
 
@@ -34,15 +47,33 @@ class  PLEGMA_EigSolverParams{
 #endif
 };
 
-class PLEGMA_EigSolver: private PLEGMA_EigSolverParams{
+class PLEGMA_EigSolver{
  private:
-  void applyOperator();
+  EigSolverParams p;
+  bool verbose;
+  int field_length;
+  size_t size_per_Vec;
+  size_t size_NeV;
+  size_t size_NkV;
+  size_t size_total;
+
+  double *h_eigVecs;
+  double *h_eigVals;
+
+  QUDA_dirac *dOp;
+
+  PLEGMA_Vector<double> *din;
+  PLEGMA_Vector<double> *dout;
+  PLEGMA_Vector<double> *tmp1;
+  PLEGMA_Vector<double> *tmp2;
+
+  void applyPolyOperator(double *out, double *in);
   void initEigSolver();
-  void computeEigVectors();
-  void computeEigValues();
+  void computeEigVecs();
+  void computeEigVals();
   
  public:
-  PLEGMA_EigSolver(PLEGMA_EigSolverParams params, bool verbose);
+  PLEGMA_EigSolver(EigSolverParams params, QudaDslashType dslashType, bool verbose=false);
   virtual ~PLEGMA_EigSolver();
   void projectVector(QKXTM_Vector<double> &vecOut, QKXTM_Vector<double> &vecIn);
 };
--- lib/PLEGMA_EigSolver.cu
+++ lib/PLEGMA_EigSolver.cu
@@ -1,3 +1,83 @@
 #include <PLEGMA_EigSolver.h>
 
+PLEGMA_EigSolver::PLEGMA_EigSolver(EigSolverParams params, QudaDslashType dslashType, bool verbose):verbose(verbose),p(params),
+												    h_eigVecs(nullptr),h_eigVals(nullptr),dOp(nullptr)
+												    d_in(nullptr),d_out(nullptr),
+												    tmp1(nullptr),tmp2(nullptr)
+												    {
+  if(p.NeV <=0 ){
+    printfQuda("Warning: Eigensolver instructed to use NeV=%d, skipping eigenvectors calculation\n",p.NeV);
+    return;
+  }
+  if(p.NkV < p.NeV) errorQuda("The NkV should be larger than NeV");
+  field_length = N_SPINS * N_COLS;
+  size_per_Vec = GK_totalVolume * field_length;
+  size_NeV = p.NeV * size_per_Vec;
+  size_NkV = p.NkV * size_per_Vec;
+  size_total = size_NeV + size_NkV;
+  try{
+    h_eigVecs = new double[size_total*2];
+    h_eigVals = new double[p.NeV*2];
+  }
+  catch (std::bad_alloc& err){
+    errorQuda(err.what());
+  }
+  
+  //start the diracOp
+  dOp = new QUDA_dirac(dslashType);
 
+  din = new PLEGMA_Vector<double>(DEVICE);
+  dout = new PLEGMA_Vector<double>(DEVICE);
+  tmp1 = new PLEGMA_Vector<double>(DEVICE);
+  tmp2 = new PLEGMA_Vector<double>(DEVICE);
+
+  initEigSolver();
+  computeEigVecs();
+  delete din;
+  delete dout;
+  delete tmp1;
+  delete tmp2;
+  computeEigVals();
+}
+
+PLEGMA_EigSolver::~PLEGMA_EigSolver(){
+  delete[] h_eigVecs;
+  delete[] h_eigVals;
+  delete dOp;
+}
+
+void PLEGMA_EigSolver::applyOperator(double *out, double *in){
+  cudaMemcpy(d_in.D_elem(),in,size_per_Vec*sizeof(double),cudaMemcpyHostToDevice);
+  if(!p.isACC) dOp->appy<MdagM>(*d_out,*d_in);
+  else{
+    double delta,theta;
+    double sigma,sigma1,sigma_old;
+    std::complex<double> d1(0.,0.),d2(0.,0.),d3(0.,0.);
+    double a = p.amin;
+    double b = p.amax;
+    delta = (b-a)/2.0;
+    theta = (b+a)/2.0;
+    sigma1 = -delta/theta;
+    d1.real() =  sigma1/delta;
+    d2.real() =  1.0;
+    dOp->appy<MdagM>(*d_out,*d_in);
+    cuBLAS::scal(size_per_Vec, d1.real, d_out->D_elem() );
+    cuBLAS::axpy(size_per_Vec, (double*) d2, d_in->D_elem(), d_out->D_elem());
+    if(p.PolyDeg > 1){
+      tmp1->copy(*d_in);
+      tmp2->copy(*d_out);
+      for(int i=2; i <= p.PolyDeg; i++){
+	sigma = 1.0/(2.0/sigma1-sigma_old);
+	d1.real = 2.0*sigma/delta;
+	d2.real = -d1.real*theta;
+	d3.real = -sigma*sigma_old;
+	dOp->apply<MdagM>(*d_out, *tmp2);
+	plegma::axpbypcz(size_per_Vec,(double*) d3,tmp1->D_elem(), (double*) d2,tmp2->D_elem(),(double*) d1,d_out->D_elem());
+	tmp1->copy(*tmp2);
+	tmp2->copy(*d_out);
+	sigma_old=sigma;
+      }
+    }
+  }
+  cudaMemcpy(out,d_out.D_elem(),size_per_Vec*sizeof(double),cudaMemcpyDeviceToHost);
+}
