e8be8d0dee382dcf8e0f02b35f40160a1e6c0006
Simone Bacchio <s.bacchio@gmail.com>
2019-02-12 14:18:56 +0200

    Adding check of Plaquette in QUDA

--- plegma/Plaquette.cpp
+++ plegma/Plaquette.cpp
@@ -9,17 +9,23 @@ int main(int argc, char **argv)
   initialize(argc, argv);
 
   // Allocation done on BOTH, DEVICE and HOST
-  PLEGMA_Gauge<double> pGauge(BOTH);
+  PLEGMA_Gauge<double> gauge(BOTH);
 
   // Reading from Lime file and loading to device
-  pGauge.readFromLime(latfile.c_str());
-  pGauge.load();
+  gauge.readFromLime(latfile.c_str());
+  gauge.load();
   
   // Compuiting plaquette on device in three different way for crosschecking
-  pGauge.calculatePlaq();
-  pGauge.calculatePlaqCorners();
-  pGauge.calculatePlaqShifts();
+  gauge.calculatePlaq();
+  gauge.calculatePlaqCorners();
+  gauge.calculatePlaqShifts();
 
+  // Loading to QUDA and computing plaquette also there
+  initGaugeQuda(gauge, false, QUDA_SU3_LINKS);
+  double plq[3]; // total, spatial and temporal plaquette
+  plaqQuda(plq);
+  printfQuda("TEST: Calculated plaquette in QUDA: %f (sp: %f, T: %f)\n", plq[0], plq[1], plq[2]);
+  
   finalize();
  
   return 0;
