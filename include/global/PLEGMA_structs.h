aff73ae25f38aaf85fd7b0387ec9a2faa2c66f3e
Simone Bacchio <s.bacchio@gmail.com>
2019-02-15 12:19:09 +0200

    Improving the structure of global_vars

--- include/global/PLEGMA_structs.h
+++ include/global/PLEGMA_structs.h
@@ -22,49 +22,77 @@ struct tex_mom_list {
   };
 };
 
+struct var_holder {
+  void * hostPointer;
+  void * devPointer;
+  int size;
+  int bytes;
+  std::string var_name;
+  std::string type_name;
+  std::string type_print;
+  
+  template<typename hostT>
+  var_holder(const char* name, hostT &host, int s=0) {
+    hostPointer = (void*) &host;
+    devPointer = NULL;
+    if(s==0) size = sizeof(host)/sizeof(hostT);
+    else size=s;
+    bytes = sizeof(hostT);
+    var_name = name;
+    type_name = plegma::type_name<hostT>();
+    type_print = type_char<hostT>();
+  }
+  template<typename hostT, typename devT>
+  var_holder(const char* name, hostT &host, devT &dev, int s=0) : var_holder(name, host, s) {
+    devPointer = (void*) &dev;
+  }
+  void copyToDevice() {
+    if(devPointer != NULL) {
+      cudaMemcpy( devPointer, hostPointer, bytes*size, cudaMemcpyHostToDevice);
+      checkCudaError();
+    }
+  }
+  void copyToDeviceConstant() {
+    if(devPointer != NULL) {
+      cudaMemcpyToSymbol( *((char**) devPointer), hostPointer, bytes*size);
+      checkCudaError();
+    }
+  }
+};
+  
 // here we collect the global variables for then running some default functions on them (print and copy to device)
 struct global_vars {
-  // globals on host only
-  std::vector<void*> host_only_pointer;
-  std::vector<int> host_only_size;
-  std::vector<size_t> host_only_bytes;
-  std::vector<char> host_only_type;
-  std::vector<std::string> host_only_type_name;
-  std::vector<std::string> host_only_name;
-    
-  // globals on both, host and device
-  std::vector<std::array<void*, 2>> both_pointer;
-  std::vector<int> both_size;
-  std::vector<size_t> both_bytes;
-  std::vector<char> both_type;
-  std::vector<std::string> both_type_name;
-  std::vector<std::string> both_name;
+  std::vector<var_holder> globals;
 
   template<typename hostT>
-  void add(const char* name, hostT &host, int size=1) {
-    host_only_pointer.push_back((void*) &host);
-    host_only_size.push_back(size);
-    host_only_bytes.push_back(sizeof(hostT)*size);
-    host_only_type.push_back(type_char<hostT>());	
-    host_only_type_name.push_back(demangle(typeid(host).name()));	
-    host_only_name.push_back(name);	
+  void add(const char* name, hostT &host, int size=0) {
+    globals.push_back(var_holder(name,host,size));
   }
   template<typename hostT, typename deviceT>
   void add(const char* name, hostT &host, deviceT &device, int size=1) {
-    both_pointer.push_back({(void*) &host, (void*) &device});
-    both_size.push_back(size);
-    both_bytes.push_back(sizeof(hostT)*size);
-    both_type.push_back(type_char<hostT>());	
-    both_type_name.push_back(type_name<hostT>());	
-    both_name.push_back(name);
+    globals.push_back(var_holder(name,host,device, size));
   }
   void copyToDevice() {
-    for(int i = 0; i != both_pointer.size(); i++) {
-      cudaMemcpyToSymbol( *((char**) both_pointer[i][1]), both_pointer[i][0], both_bytes[i]);
+    for(int i = 0; i != globals.size(); i++) {
+      globals[i].copyToDeviceConstant();
     }
-    checkCudaError();
   }
   void print() {
-
+    /*
+    printfQuda("Global constants available only on host:\n");
+    for(int i = 0; i != host_only_pointer.size(); i++) {
+      std::string line = "HGC_" + host_only_name[i] + ", type: " + host_only_type_name[i] + ", size: " + std::to_string(host_only_size[i]) + ", values:";
+      int bytes = host_only_bytes[i] / host_only_size[i]; 
+      for(int j=0; j<host_only_size[i]; j++) {
+	char * value, type[] = " %d";
+	type[2] = host_only_type[i];
+	asprintf(&value, type, *((char *) host_only_pointer[i] + j*bytes));
+	line += value;
+	free(value);
+      }
+      line+="\n";
+      printfQuda(line.c_str());
+    }
+    */
   }
 };
