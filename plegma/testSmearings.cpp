5a48483ddcc794b010fb890fa2774bdb04f4228b
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-02-21 16:26:06 +0200

    Update to read some options

--- plegma/testSmearings.cpp
+++ plegma/testSmearings.cpp
@@ -5,12 +5,18 @@ using namespace plegma;
 using namespace quda;
 
 extern int device;
-extern char latfile[];
+//extern char latfile[];
 
 int main(int argc, char **argv)
 {
-  initialize(argc, argv);
+  initialize(argc, argv, false);
+  int dimStout=3;
+  int dimAPE=3;
+  
+  opt.set("dim-stout", "Directions to do the Stout smearing. Allowed options (3,4)", verbosity, dimStout);
+  opt.set("dim-APE", "Directions to do the APE smearing. Allowed options (3,4)", verbosity, dimAPE);
 
+  HGC_options->close();
   // Allocation done on BOTH, DEVICE and HOST
   PLEGMA_Gauge<double> gauge;
 
@@ -19,10 +25,13 @@ int main(int argc, char **argv)
   gauge.load();
   gauge.calculatePlaq();
 
+
   PLEGMA_Gauge<double> smearedGauge;
-  smearedGauge.stoutSmearing(gauge, 10, 0.1, 4);
+  smearedGauge.stoutSmearing(gauge, nsmearStout, alphaStout, dimStout);
+  PLEGMA_printf("Plaquette using stout is:");
   smearedGauge.calculatePlaq();
-  smearedGauge.APEsmearing(gauge, 10, 0.1, 3);
+  smearedGauge.APEsmearing(gauge, nsmearAPE, alphaAPE, dimAPE);
+  PLEGMA_printf("Plaquette using APE is:");
   smearedGauge.calculatePlaq();
 
   finalize();
