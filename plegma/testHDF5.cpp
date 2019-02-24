a44adea77f51d0556f1a607b938300b703fb8e89
Simone Bacchio <s.bacchio@gmail.com>
2019-02-24 19:58:38 +0200

    Making test more interesting

--- plegma/testHDF5.cpp
+++ plegma/testHDF5.cpp
@@ -8,20 +8,30 @@ int main(int argc, char **argv)
 {
   initialize(argc, argv);
 
-  // This shouold create the file test.h5
-  HDF5 test("./test");
+  { 
+    // This creates the file foo.h5
+    // with directory structure /foo1/foo2/foo3/foo4/foo5
+    HDF5 test("./foo.h5/foo1");
+    test.cd("foo2/");
 
-  // This shouold open the file test.h5 and create group foo1
-  HDF5 test2("./test.h5/foo1");
+    test.cd("../foo2/foo3");
 
+    test.cd("/foo1/foo2/foo3/foo4/");
+    
+    test.cd("///foo1///./../foo1/foo2/foo3///./../../foo2/foo3/foo4/foo5");
+    PLEGMA_printf("Now I'm here: %s\n",test.pwd().c_str());    
+  }
   if (latfile != "") {
+    // This creates the file conf.h5
+    HDF5 test2("./conf");
+
     // Allocation done on BOTH, DEVICE and HOST
     PLEGMA_Gauge<double> gauge(BOTH);
-    
+      
     // Reading from Lime file and loading to device
     gauge.readFromLime(latfile.c_str());
     gauge.load();
-    
+      
     // Compuiting plaquette on device in three different way for crosschecking
     double plaq = gauge.calculatePlaq();
 
@@ -51,10 +61,10 @@ int main(int argc, char **argv)
     lshape.push_back(2);
     start.push_back(0);
     
-    test.write_dataset("gauge",gauge.H_elem(), shape, lshape, start);
-    test.write_attribute("gauge", "shape", descr);
-    test.write_attribute("gauge", "latfile", latfile);
-    test.write_attribute("gauge", "plaquette", std::to_string(plaq));
+    test2.write_dataset("gauge", gauge.H_elem(), shape, lshape, start);
+    test2.write_attribute("gauge", "shape", descr);
+    test2.write_attribute("gauge", "latfile", latfile);
+    test2.write_attribute("gauge", "plaquette", std::to_string(plaq));
   }
   
   finalize();
