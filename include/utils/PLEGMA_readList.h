161072e869c6e32518dabb5aaf98e53d44489d77
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-02-27 12:34:41 +0200

    Comment out functions reading from list tsinks and projs which is not needed

--- include/utils/PLEGMA_readList.h
+++ include/utils/PLEGMA_readList.h
@@ -25,49 +25,49 @@ inline void readSourceList() {
   }
 }
 
-inline void readTSinkList() {
-  hostMalloc(tSink, numTSink*sizeof(tSink));
-  std::ifstream file(pathListTSink.c_str(), std::ifstream::in);
-  int i=0;
-  while (!file.eof() && i<numTSink) {
-    file >> tSink[i];
-    i++;
-  }
-  file.close();
-  if(i<numTSink) {
-    PLEGMA_warning("Read only %d T sinks. Continuing with that ammount.\n", i);
-    numTSink = i;
-  }
-  if(verbosity) {
-    PLEGMA_printf("\nList of read T sink:\n");
-    for(int j=0; j<numTSink; j++) {
-	PLEGMA_printf("tsink[%d]: %d\n", j, tSink[j]);
-    }
-  }
-}
+/* inline void readTSinkList() { */
+/*   hostMalloc(tSink, numTSink*sizeof(tSink)); */
+/*   std::ifstream file(pathListTSink.c_str(), std::ifstream::in); */
+/*   int i=0; */
+/*   while (!file.eof() && i<numTSink) { */
+/*     file >> tSink[i]; */
+/*     i++; */
+/*   } */
+/*   file.close(); */
+/*   if(i<numTSink) { */
+/*     PLEGMA_warning("Read only %d T sinks. Continuing with that ammount.\n", i); */
+/*     numTSink = i; */
+/*   } */
+/*   if(verbosity) { */
+/*     PLEGMA_printf("\nList of read T sink:\n"); */
+/*     for(int j=0; j<numTSink; j++) { */
+/* 	PLEGMA_printf("tsink[%d]: %d\n", j, tSink[j]); */
+/*     } */
+/*   } */
+/* } */
 
-inline void readProjList() {
-  hostMalloc(proj, numProj*sizeof(proj));
-  std::ifstream file(pathListProj.c_str(), std::ifstream::in);
-  int i=0;
-  std::string tmpString;
-  std::string proj_str[]={"P4_P","P4G5G1_P","P4G5G3_P","P4_M","P4G5G1_M","P4G5G2_M","P4G5G3_M"};
-  while (!file.eof() && i<numProj) {
-    file >> tmpString;
-    std::string *p = std::find (proj_str, proj_str+(int)N_PROJS, tmpString);
-    proj[i] = (WHICHPROJECTOR) (int) (p - proj_str);
-    if (proj[i] == N_PROJS) PLEGMA_error("Proj %s not found. Options are P4_P, P4G5G1_P, P4G5G2_P, P4G5G3_P, P4_M, P4G5G1_M, P4G5G2_M, P4G5G3_M.\n");
-    i++;
-  }
-  file.close();
-  if(i<numProj) {
-    PLEGMA_warning("Read only %d projectors. Continuing with that ammount.\n", i);
-    numProj = i;
-  }
-  if(verbosity) {
-    PLEGMA_printf("\nList of read projectors:\n");
-    for(int j=0; j<numProj; j++) {
-      PLEGMA_printf("proj[%d]: %d\n", j, proj_str[(int)proj[j]]);
-    }
-  }
-}
+/* inline void readProjList() { */
+/*   hostMalloc(proj, numProj*sizeof(proj)); */
+/*   std::ifstream file(pathListProj.c_str(), std::ifstream::in); */
+/*   int i=0; */
+/*   std::string tmpString; */
+/*   std::string proj_str[]={"P4_P","P4G5G1_P", "P4G5G2_P","P4G5G3_P","P4_M","P4G5G1_M","P4G5G2_M","P4G5G3_M"}; */
+/*   while (!file.eof() && i<numProj) { */
+/*     file >> tmpString; */
+/*     std::string *p = std::find (proj_str, proj_str+(int)N_PROJS, tmpString); */
+/*     proj[i] = (WHICHPROJECTOR) (int) (p - proj_str); */
+/*     if (proj[i] == N_PROJS) PLEGMA_error("Proj %s not found. Options are P4_P, P4G5G1_P, P4G5G2_P, P4G5G3_P, P4_M, P4G5G1_M, P4G5G2_M, P4G5G3_M.\n"); */
+/*     i++; */
+/*   } */
+/*   file.close(); */
+/*   if(i<numProj) { */
+/*     PLEGMA_warning("Read only %d projectors. Continuing with that ammount.\n", i); */
+/*     numProj = i; */
+/*   } */
+/*   if(verbosity) { */
+/*     PLEGMA_printf("\nList of read projectors:\n"); */
+/*     for(int j=0; j<numProj; j++) { */
+/*       PLEGMA_printf("proj[%d]: %d\n", j, proj_str[(int)proj[j]]); */
+/*     } */
+/*   } */
+/* } */
