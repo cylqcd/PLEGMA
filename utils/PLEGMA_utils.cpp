756aa7cea176f7ecb2e76c0dfb418402af30dd06
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-03-14 15:56:36 +0200

    Put scan of r2 also in negative region to do counting correctly

--- utils/PLEGMA_utils.cpp
+++ utils/PLEGMA_utils.cpp
@@ -192,9 +192,9 @@ template void applyBoundaryConditions<float>(PLEGMA_Gauge<float> &gauge, bool an
 std::vector<int> createR2(std::vector<int> &vec){
   if(!HGC_init_PLEGMA_flag) PLEGMA_error("Initialize PLEGMA first");
   if(vec.size() != 0) PLEGMA_error("The vector provided is not empty");
-  for(int xx = 0; xx < HGC_totalL[0]/2; xx++)
-    for(int yy = 0; yy < HGC_totalL[1]/2; yy++)
-      for(int zz = 0; zz < HGC_totalL[2]/2; zz++)
+  for(int xx = -HGC_totalL[0]/2; xx < HGC_totalL[0]/2; xx++)
+    for(int yy = -HGC_totalL[1]/2; yy < HGC_totalL[1]/2; yy++)
+      for(int zz = -HGC_totalL[2]/2; zz < HGC_totalL[2]/2; zz++)
 	vec.push_back(xx*xx + yy*yy + zz*zz);
   return clearDuplicates(vec);
 }
