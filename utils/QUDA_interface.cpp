6a811e2bcc543e3945b15d043cf12f27c0925ed0
Simone Bacchio <s.bacchio@gmail.com>
2019-04-17 16:09:50 +0300

    Updating initComms with quda version

--- utils/QUDA_interface.cpp
+++ utils/QUDA_interface.cpp
@@ -40,21 +40,14 @@ static void initRand()
 
 void initComms(int argc, char **argv, const int *commDims)
 {
+  // TODO: QMP not supported right now
 #if defined(QMP_COMMS)
   QMP_thread_level_t tl;
   QMP_init_msg_passing(&argc, &argv, QMP_THREAD_SINGLE, &tl);
 
-  // FIXME? - tests crash without this
   QMP_declare_logical_topology(commDims, 4);
-
 #elif defined(MPI_COMMS)
-#ifdef PTHREADS
-  int provided;
-  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
-#else
   MPI_Init(&argc, &argv);
-#endif
-
 #endif
   initCommsGridQuda(4, commDims, NULL, NULL);
   initRand();
