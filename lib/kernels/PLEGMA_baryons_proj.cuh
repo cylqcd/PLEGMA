a49ae0d1a11d4a2a29e0ec5efa9b553e022f9357
Simone Bacchio <s.bacchio@gmail.com>
2019-04-19 14:44:06 +0300

    renaming file to PLEGMA_baryons_proj

---
+++ lib/kernels/PLEGMA_baryons_proj.cuh
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <PLEGMA_kernel_utils.cuh>
+#include <PLEGMA_baryons_proj_arrays.cuh>
+
+template<typename FloatA, typename FloatC>
+void contract_baryons_proj(propTex<FloatA> texPropUP, propTex<FloatA> texPropDN,
+			   propTex<FloatA> texPropST, propTex<FloatA> texPropCH,
+			   PLEGMA_Correlator<FloatC> &corr, int it, std::vector<int> &todo);
+
