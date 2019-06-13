0aa4103c76967c40cf46294625fd07ae43d0eef1
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-06-13 10:15:17 +0300

    Remove pragma unroll because it might has undefined behavior if number of unrolls is input

--- lib/kernels/PLEGMA_shifts.cuh
+++ lib/kernels/PLEGMA_shifts.cuh
@@ -7,12 +7,12 @@ static __global__ void shifts_kernel(Float *in, Float *out, int length_field, in
   if(sid >= DGC_localVolume) return;
   generic2<Float> R(in);
   Float2<Float> *out2 = (Float2<Float> *) out;
-#pragma unroll
   for(int i = 0 ; i < length_field ; i++){
     Float2<Float> tmp = (dirOr<4) ? R.get<Minus>(i,sid,length_field,dirOr%4) : R.get<Plus>(i,sid,length_field,dirOr%4);
     out2[i*DGC_localVolume + sid] = tmp;
   }
 }
+
 
 template<typename Float>
 static void shiftField(PLEGMA_Field<Float> &Fin, PLEGMA_Field<Float> &Fout, int dirOr){
