cebbc0b6271ec2653a6b1e30055441b9819d4d8f
Davide Nole <davide.nole93@gmail.com>
2019-06-27 12:12:46 +0300

    using MPI_Wtime

--- plegma/Calc_2pt.cpp
+++ plegma/Calc_2pt.cpp
@@ -50,14 +50,14 @@ int main(int argc, char **argv)
 	PLEGMA_Vector<double> vectorInOut, vectorAuxD;
 	PLEGMA_Vector<float> vectorAuxF;
 	vectorAuxD.pointSource(sourcePositions[isource], isc/3, isc%3, DEVICE);
-	start_time = std::time(NULL);
+	start_time = MPI_Wtime();
 	vectorInOut.gaussianSmearing(vectorAuxD, smearedGauge, nsmearGauss, alphaGauss);
-	tmp_time += std::time(NULL)-start_time;
+	tmp_time += MPI_Wtime()-start_time;
 	PLEGMA_printf("Going to invert UP for component %d\n", isc);
 	solver.solve(vectorInOut, vectorInOut);
-	start_time = std::time(NULL);
+	start_time = MPI_Wtime();
 	vectorAuxD.gaussianSmearing(vectorInOut, smearedGauge, nsmearGauss, alphaGauss);
-	tmp_time += std::time(NULL)-start_time;
+	tmp_time += MPI_Wtime()-start_time;
 	vectorAuxF.copy(vectorAuxD);
 	propUP.absorb(vectorAuxF, isc/3, isc%3);
       }	
@@ -72,14 +72,14 @@ int main(int argc, char **argv)
 	PLEGMA_Vector<double> vectorInOut, vectorAuxD;
 	PLEGMA_Vector<float> vectorAuxF;
 	vectorAuxD.pointSource(sourcePositions[isource], isc/3, isc%3, DEVICE);
-	start_time = std::time(NULL);
+	start_time = MPI_Wtime();
 	vectorInOut.gaussianSmearing(vectorAuxD, smearedGauge, nsmearGauss, alphaGauss);
-	tmp_time += std::time(NULL)-start_time;
+	tmp_time += MPI_Wtime()-start_time;
 	PLEGMA_printf("Going to invert DN for component %d\n", isc);
 	solver.solve(vectorInOut, vectorInOut);
-	start_time = std::time(NULL);
+	start_time = MPI_Wtime();
 	vectorAuxD.gaussianSmearing(vectorInOut,smearedGauge, nsmearGauss, alphaGauss);
-	tmp_time += std::time(NULL)-start_time;
+	tmp_time += MPI_Wtime()-start_time;
 	vectorAuxF.copy(vectorAuxD);
 	propDN.absorb(vectorAuxF, isc/3, isc%3);
       }
@@ -90,14 +90,14 @@ int main(int argc, char **argv)
       propDN.applyBoundaries_device(sourcePositions[isource][3]);
       
       PLEGMA_Correlator<float> corr(corr_space, maxQsq);
-      start_time = std::time(NULL);
+      start_time = MPI_Wtime();
       corr.contractMesons(propUP, propDN, sourcePositions[isource]);
-      tmp_time = std::time(NULL)-start_time;
+      tmp_time = MPI_Wtime()-start_time;
       corr.writeFile(twop_filename.c_str(), corr_file_format);
 
-      start_time = std::time(NULL);
+      start_time = MPI_Wtime();
       corr.contractBaryons(propUP, propDN, sourcePositions[isource]);
-      tmp_time += std::time(NULL)-start_time;
+      tmp_time += MPI_Wtime()-start_time;
       corr.writeFile(twop_filename.c_str(), corr_file_format);
       PLEGMA_printf("Contraction time %lf sec\n",tmp_time);
     }
