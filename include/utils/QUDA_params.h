446665953320f8db76ec6d02e31334d7facdbae8
fmani <floriano.manigrasso@gmail.com>
2019-07-22 20:32:14 +0300

    Added parameter list exact deflation

--- include/utils/QUDA_params.h
+++ include/utils/QUDA_params.h
@@ -1,4 +1,3 @@
-//-----------------//
 // QUDA Parameters //
 //-----------------//
 #ifdef ALLOCATE
@@ -77,6 +76,24 @@ define(double smoother_tol[QUDA_MAX_MG_LEVEL], {});
 define(int coarse_solver_maxiter[QUDA_MAX_MG_LEVEL], {});
 define(QudaSchwarzType schwarz_type[QUDA_MAX_MG_LEVEL], {});
 define(int schwarz_cycle[QUDA_MAX_MG_LEVEL], {});
+define(QudaBoolean mg_eig[QUDA_MAX_MG_LEVEL], {});
+define(int mg_eig_nEv[QUDA_MAX_MG_LEVEL], {});
+define(int mg_eig_nKr[QUDA_MAX_MG_LEVEL], {});
+define(QudaBoolean  mg_eig_require_convergence[QUDA_MAX_MG_LEVEL], {});
+define(int mg_eig_check_interval[QUDA_MAX_MG_LEVEL], {});
+define(int mg_eig_max_restarts[QUDA_MAX_MG_LEVEL], {}); 
+define(QudaBoolean mg_eig_use_normop[QUDA_MAX_MG_LEVEL], {});
+define(QudaBoolean mg_eig_use_dagger[QUDA_MAX_MG_LEVEL], {});
+define(double mg_eig_tol[QUDA_MAX_MG_LEVEL], {}); 
+define(QudaBoolean mg_eig_use_poly_acc[QUDA_MAX_MG_LEVEL], {});
+define(int mg_eig_poly_deg[QUDA_MAX_MG_LEVEL], {});
+define(double mg_eig_amin[QUDA_MAX_MG_LEVEL], {});
+define(double mg_eig_amax[QUDA_MAX_MG_LEVEL], {});
+define(QudaEigSpectrumType mg_eig_spectrum[QUDA_MAX_MG_LEVEL], {});
+define(QudaBoolean mg_eig_coarse_guess, {});
+define(QudaEigType mg_eig_type[QUDA_MAX_MG_LEVEL], {});
+
+
 define(std::string vec_infile, "");
 define(std::string vec_outfile, "");
 
