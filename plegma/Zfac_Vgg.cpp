0c030fd809e07229b3213d391a8997f90e604449
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2019-09-25 17:03:32 +0300

    Update Zgg fixing repeating zero stout step

--- plegma/Zfac_Vgg.cpp
+++ plegma/Zfac_Vgg.cpp
@@ -45,6 +45,8 @@ int main(int argc, char **argv){
   HGC_options->set("isGFixed", "If this is true it means that the configuration provided is alread gauge fixed", verbosity,isGFixed);
   bool doGLoops = true;
   HGC_options->set("doGLoops", "If you want to compute also gluon loops", verbosity,doGLoops);
+  bool doTwistTmp = true;
+  HGC_options->set("doTwistTmp", "If we want to twist momenta in temporal direction", verbosity,doTwistTmp);
   //==========================//
   initializePLEGMA();
 
@@ -59,10 +61,18 @@ int main(int argc, char **argv){
   for(int n=0; n<=nsmearStout;n++) nScount.push_back(n);
   std::vector<std::vector<std::complex<double>>> AxA;
   PLEGMA_FT<double> *ftAl=nullptr, *ftAr=nullptr;
-  std::vector<double> twistF = {0.,0.,0.,0.}; // do not put any twist in the momentum
-  std::string filenameGprop = filesPrefix + "/gProps_" + filesSuffix + ".txt";
+  std::vector<double> twistF = {0.,0.,0.,0.};
+  if(doTwistTmp) twistF[3] = 0.5;//
+  std::vector<std::string> filenameGprop;
+  for(int im=0; im < numSourcePositions; im++){
+    std::string momStr = "_px" + std::to_string(sourcePositions[im][0]) +
+      "_py" + std::to_string(sourcePositions[im][1]) +
+      "_pz" + std::to_string(sourcePositions[im][2]) +
+      "_pt" + std::to_string(sourcePositions[im][3])+"_"; 
+    filenameGprop.push_back(filesPrefix + "/gProps" + momStr + filesSuffix + ".txt");
+    if(comm_rank()== 0) cleanFile(filenameGprop[im]);
+  }  
   std::string filenameGLoops = filesPrefix + "/gLoops_" + filesSuffix +".txt";
-  if(comm_rank()== 0) cleanFile(filenameGprop);
   if(doGLoops) if(comm_rank() == 0) cleanFile(filenameGLoops);
   std::vector<int> muVec,nuVec, c1Vec, c2Vec;
   for(int mu = 0; mu < N_DIMS; mu++)
@@ -92,7 +102,6 @@ int main(int argc, char **argv){
     }
     
     // Here we need to compute the gluon propagator which is located at gauge2 
-    //std::vector<double> twistF = {0,0,0,0}; // twist in the temporal direction
     AxA.clear();    
     for(int im=0; im < numSourcePositions; im++){
       std::vector<double> mom=(std::vector<double>) {sourcePositions[im][0]+twistF[0],
@@ -110,7 +119,7 @@ int main(int argc, char **argv){
       ftAr->apply(gauge2,FT_GEMV,+1); // remember to put the twist in the temporal direction
       computeGprop(AxA,*ftAl,*ftAr,mom);
 
-      if(comm_rank() == 0) write_std_vecs(filenameGprop,true,confVec,momVecX,momVecY,momVecZ,momVecT,muVec,nuVec,c1Vec,c2Vec,AxA[im]);
+      if(comm_rank() == 0) write_std_vecs(filenameGprop[im],true,confVec,momVecX,momVecY,momVecZ,momVecT,muVec,nuVec,c1Vec,c2Vec,AxA[im]);
       delete ftAl,ftAr;
     }
     // Here we compute the gluon loops
@@ -125,29 +134,29 @@ int main(int argc, char **argv){
       // gauge1 holds the link variables in landau gauge (gluon field is not needed anymore and will be used for tmp)
       gLoop.clear();
       for(int n=0; n<=nsmearStout;n++){
-	if(n%2 == 0){
-	  if(n==0) gauge2.copy(gauge1);
-	  else gauge2.stoutSmearing(gauge1,1,alphaStout,4);
-	}
-	else{
-	  gauge1.stoutSmearing(gauge2,1,alphaStout,4);
-	}
+  	if(n%2 == 0){
+  	  if(n==0) gauge2.copy(gauge1);
+  	  else gauge2.stoutSmearing(gauge1,1,alphaStout,4);
+  	}
+  	else{
+  	  gauge1.stoutSmearing(gauge2,1,alphaStout,4);
+  	}
 
-	trace2.zero_device();
-	for(int i = 0 ; i < N_DIMS-1; i++){
-	  trace1.trPmunu((n%2==0)?gauge1:gauge2, std::make_pair(3,i));
-	  trace2.axpy(trace1,(std::complex<double>) {1.,0.});
-	}
-	ftUL1.apply(trace2,FT_GEMV);
+  	trace2.zero_device();
+  	for(int i = 0 ; i < N_DIMS-1; i++){
+  	  trace1.trPmunu((n%2==0)?gauge2:gauge1, std::make_pair(3,i));
+  	  trace2.axpy(trace1,(std::complex<double>) {1.,0.});
+  	}
+  	ftUL1.apply(trace2,FT_GEMV);
 
-	trace2.zero_device();
-	for(int i = 0 ; i < N_DIMS-1; i++)
-	  for(int j = i+1 ; j < N_DIMS-1; j++){
-	    trace1.trPmunu((n%2==0)?gauge1:gauge2, std::make_pair(i,j));
-	    trace2.axpy(trace1,(std::complex<double>) {1.,0.});
-	  }
-	ftUL2.apply(trace2,FT_GEMV);
-	gLoop.push_back(ftUL1.H_elem()[0] - ftUL2.H_elem()[0]);
+  	trace2.zero_device();
+  	for(int i = 0 ; i < N_DIMS-1; i++)
+  	  for(int j = i+1 ; j < N_DIMS-1; j++){
+  	    trace1.trPmunu((n%2==0)?gauge2:gauge1, std::make_pair(i,j));
+  	    trace2.axpy(trace1,(std::complex<double>) {1.,0.});
+  	  }
+  	ftUL2.apply(trace2,FT_GEMV);
+  	gLoop.push_back(ftUL1.H_elem()[0] - ftUL2.H_elem()[0]);
       }
       std::vector<std::string> confVec(nsmearStout+1,confStr);
       if(comm_rank() == 0) write_std_vecs(filenameGLoops,true,confVec,nScount,gLoop);
@@ -155,7 +164,45 @@ int main(int argc, char **argv){
       PLEGMA_printf("conf.%s completed in %f secs\n",confStr.c_str(),t2-t1);
     }
   }  
-  // Vertex function will be create later from a multiplication of the gluon Loop with the gluon propagator
-  
+  // Vertex function will be create later from a multiplication of the gluon Loop with the gluon propagator    
   finalize();
 }
+
+
+
+
+  //   std::vector<double> gLoop00,gLoop11,gLoop22;
+
+  //   if(doGLoops){
+  //     // gauge1 holds the link variables in landau gauge (gluon field is not needed anymore and will be used for tmp)
+  //     gLoop00.clear();      gLoop11.clear();      gLoop22.clear(); gLoop.clear();
+  //     for(int n=0; n<=nsmearStout;n++){
+  // 	if(n%2 == 0){
+  // 	  if(n==0) gauge2.copy(gauge1);
+  // 	  else gauge2.stoutSmearing(gauge1,1,alphaStout,4);
+  // 	}
+  // 	else{
+  // 	  gauge1.stoutSmearing(gauge2,1,alphaStout,4);
+  // 	}
+
+  // 	for(int nu =0; nu < N_DIMS; nu++){
+  // 	  trace2.zero_device();
+  // 	  for(int i = 0 ; i < N_DIMS; i++){
+  // 	    if(i != nu){
+  // 	      trace1.trPmunu((n%2==0)?gauge2:gauge1, std::make_pair(nu,i));
+  // 	      trace2.axpy(trace1,(std::complex<double>) {1.,0.});
+  // 	    }
+  // 	  }
+  // 	  ftUL1.apply(trace2,FT_GEMV);
+  // 	  if(nu == 0) gLoop00.push_back(ftUL1.H_elem()[0]);
+  // 	  else if(nu==1) gLoop11.push_back(ftUL1.H_elem()[0]);
+  // 	  else if(nu==2)gLoop22.push_back(ftUL1.H_elem()[0]);
+  // 	  else gLoop.push_back(ftUL1.H_elem()[0]);
+  // 	}	
+  //     }
+  //     std::vector<std::string> confVec(nsmearStout+1,confStr);
+  //     if(comm_rank() == 0) write_std_vecs(filenameGLoops,true,confVec,nScount,gLoop00,gLoop11,gLoop22,gLoop);
+  //     double t2=MPI_Wtime();
+  //     PLEGMA_printf("conf.%s completed in %f secs\n",confStr.c_str(),t2-t1);
+  //   }
+  // }
