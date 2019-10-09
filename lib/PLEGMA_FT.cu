6289327ed528e80782ef47a9bf2c561d450ea2d4
Simone Bacchio <s.bacchio@gmail.com>
2019-10-09 12:13:45 +0300

    solving wrong warning

--- lib/PLEGMA_FT.cu
+++ lib/PLEGMA_FT.cu
@@ -117,7 +117,7 @@ tex_mom_list PLEGMA_FT<Float>::getTexMomList() {
     Float intp;
     for(int i=0; i<texMomList.Nmoms; i++) {
       for(int j=0; j<dims; j++) {
-	if(std::modf(momList[i][j],&intp) == 0.0) PLEGMA_warning("Function getTexMomList expects integers momenta but non integers are given");
+      	if(std::modf(momList[i][j],&intp) != 0.0) PLEGMA_warning("Function getTexMomList expects integers momenta but non integers are given");
 	hostPtr[i*4+j]=(int) round(momList[i][j]);
       }
     }
