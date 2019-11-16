06337033e8a9ef1273e5c8050b5b5125564405f1
Simone Bacchio <s.bacchio@gmail.com>
2019-11-16 16:01:17 +0200

    Solving compilation errors

--- plegma/qLoops.cpp
+++ plegma/qLoops.cpp
@@ -255,7 +255,7 @@ int main(int argc, char **argv)
 	  else if(!spinColorDil && k_probing>0){ sourceDil->applyHpropColoring4D(source,*hprop,ih,indDof);}
 	  if(spinColorDil || k_probing>0) solverDN->solve(phi,*sourceDil); else solverDN->solve(phi,source);
 	  // for convention reasons for quark loops we put the normalization factors of the fields later in the analysis
-	  phi.scaleVector(1./(2.*inv_params.kappa));
+	  phi.scale(1./(2.*inv_params.kappa));
 	  double t1=MPI_Wtime();
 	  // !!!!!!!!!!!!!!!!!!!! if we use deflation here I think we need to project solution vector but check
 #if defined(HAVE_EIGENSOLVER)
