b27b6b08ad30d0654a6f28909ff25ffd7bb4515e
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2020-01-27 12:13:13 +0200

    Fix bug in accessing su3 from Fmunu

--- lib/kernels/PLEGMA_field_utils.cuh
+++ lib/kernels/PLEGMA_field_utils.cuh
@@ -286,8 +286,8 @@ static void traceMulFmunuSu3FmunuSu3_k(PLEGMA_Field<Float> &F, PLEGMA_Fmunu<Floa
   assert(F.checkVolume(A,B,C,D));
   long int lshift = ((long int) A.munuToIndx(munu_l)) * N_COLS * N_COLS * HGC_localVolume * 2;
   long int rshift = ((long int) C.munuToIndx(munu_r)) * N_COLS * N_COLS * HGC_localVolume * 2;
-  su3_2<Float> RA((Float2<Float>*) A.D_elem()+lshift, A.Field_length(), A.is4D(), false);
-  su3_2<Float> RC((Float2<Float>*) C.D_elem()+rshift, C.Field_length(), C.is4D(), false);
+  su3_2<Float> RA((Float2<Float>*) A.D_elem()+lshift, B.Field_length(), A.is4D(), false);
+  su3_2<Float> RC((Float2<Float>*) C.D_elem()+rshift, B.Field_length(), C.is4D(), false);
 
   ProfileStruct ps(RA.volume());
   tuneAndRun(ps, "traceMulFmunuSu3FmunuSu3_kernel", traceMulFmunuSu3FmunuSu3_kernel<Float,FloatA,FloatB,FloatC,FloatD>, F.D_elem(), RA, toField2<su3_2>(B), RC, toField2<su3_2>(D));
