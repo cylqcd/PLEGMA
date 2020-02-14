eee9fd053637127b0532754d26df2dcdaba73468
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2020-02-14 10:43:53 +0200

    Getting compiled

--- plegma/piNdiagrams.cpp
+++ plegma/piNdiagrams.cpp
@@ -371,7 +371,7 @@ int main(int argc, char **argv)
           reductionsT1triangle.writeHDF5("T5sourceforT");
 
           outfilename="Tdiagramm_Antonino";
-          diagramm.T_diagramms(filtered_sourcemomentumList, reductionsT1triangle, reductionsT3triangle, reductionsT5triangle, gamma_i2, glist_sink_nucleon_unpaired, outfilename);
+          diagramm.T_diagramms(filtered_sourcemomentumList, reductionsT1triangle, reductionsT3triangle, reductionsT5triangle, gamma_i2, glist_source_nucleon_unpaired, glist_sink_nucleon_unpaired,  outfilename);
 
 
           //Compute Diagram B1 and B2 
