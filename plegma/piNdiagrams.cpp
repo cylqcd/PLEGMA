ec75a419978ef6d5d1bbbb07c88745f85eb9958b
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2020-02-21 09:49:53 +0200

    remove comments, correcting one more typo

--- plegma/piNdiagrams.cpp
+++ plegma/piNdiagrams.cpp
@@ -147,6 +147,7 @@ int main(int argc, char **argv)
 
     //Step(6) We rotate back the propagator to the physical basis
     vectorAuxD1.rotateToPhysicalBasis(vectorAuxD2,+1);
+
     //Step(7) Smearing all the time slice in the propagator
     vectorAuxD2.gaussianSmearing(vectorAuxD1, smearedGauge, nsmearGauss, alphaGauss );
     
@@ -373,7 +374,7 @@ int main(int argc, char **argv)
           propDN3D.mulMomentumPhases(momentum_i2,1);
 
           //Computing sequential propagators UD T_fii with insertion
-          //gamma4 and momentum SinkMom
+          //gamma_i2 and momentum SinkMom
           for(int isc = 0 ; isc < 12 ; isc++){
             PLEGMA_Vector<double> vectorInOut;
             PLEGMA_Vector<float> vectorAuxF;
@@ -442,17 +443,8 @@ int main(int argc, char **argv)
           outfilename="Bdiagramm_Antonino" ;
           diagramm.B_diagramms(filtered_sourcemomentumList, reductionsV3, reductionsV2, glist_ext_source, glist_ext_sink, gamma_i2, glist_source_nucleon, outfilename);
 
-     
-          //diagramm.B_diagramms(glist_source_nucleon, reductionsV3, reductionsV2, 1 );
-          //diagramm.writeHDF5("B1Diagramm");
-
-          //diagramm.B_diagramms(glist_source_nucleon, reductionsV3, reductionsV2, 2 );
-          //diagramm.writeHDF5("B2Diagramm");
-
           //Compute Diagram W1,W2
            
-          //PLEGMA_ScattCorrelator<float> diagramW(MOMENTUM_SPACE, sinkMom_Meson);
-
           reductionsV3.V3( vectorStoc_propag, glist_sink_meson, propUP);
           reductionsV3.writeHDF5("V3sourceforW12");
           reductionsV2.V2( vectorStoc_source, glist_sink_nucleon, propUP, propUPDN);
@@ -463,14 +455,9 @@ int main(int argc, char **argv)
 
           diagramm.W_diagramms(filtered_sourcemomentumList, reductionsV3, reductionsV2, glist_ext_source, glist_ext_sink, gamma_i2, glist_source_nucleon, outfilename, 2);
 
-          //diagramW.W_diagramms(glist_source_nucleon, reductionsV3, reductionsV2, 1 );
-          //diagramW.writeHDF5("W1Diagramm");
-
-          //diagramW.W_diagramms(glist_source_nucleon, reductionsV3, reductionsV2, 2 );
-          //diagramW.writeHDF5("W2Diagramm");
 
           //Compute Diagram W3,W4
-          //reductionsV3.V3( vectorStoc_propag, glist_sink_meson, propUP); maybe does not have to be recomputed
+          //
           reductionsV2.V2( vectorStoc_source, glist_sink_nucleon, propUPDN, propUP);
           reductionsV2.writeHDF5("V2sourceforW34");
 
@@ -478,12 +465,6 @@ int main(int argc, char **argv)
 
           diagramm.W_diagramms(filtered_sourcemomentumList, reductionsV3, reductionsV2, glist_ext_source, glist_ext_sink, gamma_i2, glist_source_nucleon, outfilename, 4);
 
-          //diagramW.W_diagramms(glist_source_nucleon, reductionsV3, reductionsV2, 3 );
-          //diagramW.writeHDF5("W3Diagramm");
-
-          //diagramW.W_diagramms(glist_source_nucleon, reductionsV3, reductionsV2, 4 );
-          //diagramW.writeHDF5("W4Diagramm");          
-          //
        } //loop over gamma i2
          
        std::string outfilename="Zdiagramm_Antonino" ;
@@ -552,7 +533,7 @@ int main(int argc, char **argv)
          vectorInOut.gaussianSmearing(vectorAuxD, smearedGauge, nsmearGauss, alphaGauss);
 
          //Saving the propagator
-         stochastic_propagator_momp_i2[spinindex].copy(vectorAuxD);
+         stochastic_propagator_momp_i2[spinindex].copy(vectorInOut);
 
          //tmp_time += MPI_Wtime()-start_time;         
          stochastic_propagator_momp_i2[spinindex].writeLIME(outfile_V+"propagator_fini_momentum"+std::to_string(spinindex));
