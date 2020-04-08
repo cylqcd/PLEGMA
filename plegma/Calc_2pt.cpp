a1329f1a9df64f2c1cd60773c24d49350b821db1
Simone Bacchio <s.bacchio@gmail.com>
2020-04-08 14:59:28 +0300

    Bug fix

--- plegma/Calc_2pt.cpp
+++ plegma/Calc_2pt.cpp
@@ -130,19 +130,19 @@ int main(int argc, char **argv)
       PLEGMA_Propagator<float> propS[nSmaller];
       for(int ismall=0; ismall < nSmaller; ismall++) {
 	for(int i=0;i<QUDA_MAX_MG_LEVEL;i++) mu_factor[i] = 1;
-	mu = (cSmaller=='s') ? mu_s[ismall] : mu_c[ismall];
+	double run_mu = (cSmaller=='s') ? mu_s[ismall] : mu_c[ismall];
 	int nsmear = (cSmaller=='s') ? nsmearGauss_s : nsmearGauss_c;
 	
-	TIME(computePropagator(propS[ismall], mu, (cSmaller=='s') ? STRANGE : CHARM, nsmear));
+	TIME(computePropagator(propS[ismall], run_mu, (cSmaller=='s') ? STRANGE : CHARM, nsmear));
       }
       
       int nLarger = (cSmaller!='s') ? mu_s.size() : mu_c.size();
       if(nLarger > 0) {
 	PLEGMA_Propagator<float> propL;
 	for(int ilarge=0; ilarge < nLarger; ilarge++) {
-	  mu = (cSmaller!='s') ? mu_s[ilarge] : mu_c[ilarge];
+	  double run_mu = (cSmaller!='s') ? mu_s[ilarge] : mu_c[ilarge];
 	  int nsmear = (cSmaller!='s') ? nsmearGauss_s : nsmearGauss_c;
-	  TIME(computePropagator(propL, mu, (cSmaller!='s') ? STRANGE : CHARM, nsmear));
+	  TIME(computePropagator(propL, run_mu, (cSmaller!='s') ? STRANGE : CHARM, nsmear));
 	  
 	  if(nSmaller>0) {
 	    for(int ismall=0; ismall < nSmaller; ismall++) {
