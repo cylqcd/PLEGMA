7850c0a25113ddb6a4a4dfe1574abbeab32a9136
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2020-08-24 17:23:29 +0300

    correcting B4 and B6

--- lib/PLEGMA_ScattCorrelator.cu
+++ lib/PLEGMA_ScattCorrelator.cu
@@ -931,13 +931,13 @@ void PLEGMA_ScattCorrelator<Float>::B_diagramms(PLEGMA_ScattCorrelator<Float> &s
       this->V3V2reduction(        srcV3, srcV2, 0, false, ig_i2, true,  factor, false); //checked FP
       break;
     case 4:
-      this->V3V2reduction(        srcV3, srcV2, 2, false, ig_i2, true,  factor, true); //checked FP
+      this->V3V2reduction(        srcV3, srcV2, 2, false, ig_i2, true,  factor, false); //checked FP
       break;
     case 5:
       this->V3V2reduction_matrix( srcV3, srcV2, 1, false, ig_i2, true,  factor, false); //checked FP
       break;
     case 6:
-      this->V3V2reduction_matrix( srcV3, srcV2, 0, false, ig_i2, false,  factor, true); //checked FP
+      this->V3V2reduction_matrix( srcV3, srcV2, 0, false, ig_i2, true,  factor, false); //checked FP
       break;
     case 7:
       this->V3V2reduction(        srcV3, srcV2, 2, true,  ig_i2, false, factor, true);//checked FP
