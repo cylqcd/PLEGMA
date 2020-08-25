c893b19c292f471ce3c2b47a6808c4261d08f81c
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2020-08-25 13:16:35 +0300

    removing reductions_DD_V2_GAMMAF1D_U

--- include/PLEGMA_ScattCorrelator.h
+++ include/PLEGMA_ScattCorrelator.h
@@ -125,8 +125,8 @@ namespace plegma {
      *         propagators. It is used for forming diagrams for 2 hadron 2pt correlation function where the sink to sink 
      *         propagator is replaced by a stochastic one
      *         Formula
-     *         V4_{Gamma}^{alpha0,alpha1,alpha2}_{l}=\eps_{abc}\eps_{lmn}\phi^{alpha0}_{a}S1^{beta1,alpha1}_{b,m}\Gamma^t^{beta1,beta0}*
-     *                                               S2^{beta1,alpha2}
+     *         V4_{Gamma}^{alpha0,alpha1,alpha2}_{l}=\eps_{abc}\eps_{lmn}\phi^{alpha0}_{a}S1^{beta1,alpha1}_{b,m}\Gamma{beta0,beta1}*
+     *                                               S2^{beta0,alpha2}
      *  @params PLEGMA_Vector<Float> &Phi When the propagator that should be replaced is a DD type, then this should be gamma_5 *
      *                                    stochastic propagator, when it is UU type then is should be the stochastic source itself
      *                                    without gamma_5 multiplication
