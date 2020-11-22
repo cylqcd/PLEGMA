0470ffc1f68e8b1287cdf55e92a91713a7099d19
Simone Bacchio <s.bacchio@gmail.com>
2020-11-22 18:05:50 +0200

    Resoring double precision

--- plegma/Zfac.cpp
+++ plegma/Zfac.cpp
@@ -19,24 +19,21 @@ int main(int argc, char **argv)
 
   // Read the configurations //
   // Warning gauge fixing is needed //
-  PLEGMA_Gauge<double> gauge;
+  PLEGMA_Gauge<double> gauge(BOTH,FIRST_VERTEX);
   gauge.readFile(latfile, LIME_FORMAT);
   gauge.calculatePlaq();
   initGaugeQuda(gauge, boundaryCond == "antiperiodic");
   plaqQuda();
   std::string confStr=splitStrFwd(latfile,'.');
   if(boundaryCond == "antiperiodic") applyBoundaryConditions(gauge,true);
-  PLEGMA_Gauge<float> fgauge(BOTH,FIRST_VERTEX);
-  fgauge.copy(gauge);
   /////////////////////////////
 
   if(mu < 0) mu = -mu; // we invert only for up quark with UKQCD conventions
   QUDA_solver solver(mu);
   PLEGMA_Vector<double> vecIn;
   PLEGMA_Vector<double> vecOut;
-  PLEGMA_Vector<float> vecAux;
-  PLEGMA_Propagator<float> rprop(BOTH,FIRST_VERTEX);
-  PLEGMA_Propagator<float> lprop(BOTH,FIRST_VERTEX);
+  PLEGMA_Propagator<double> rprop(BOTH,FIRST_VERTEX);
+  PLEGMA_Propagator<double> lprop(BOTH,FIRST_VERTEX);
   for(int i = 0; i < numSourcePositions; i++){ // iterate over the momenta we want to do
     site& sS = sourcePositions[i];
     std::vector<int> mom= {sS[0],sS[1],sS[2],sS[3]};
@@ -56,12 +53,11 @@ int main(int argc, char **argv)
       solver.solve(vecOut,vecIn);
       vecOut.apply_gamma(G5,LEFT);
       vecOut.apply_gamma(G4,LEFT);
-      vecAux.copy(vecOut);
-      rprop.absorb(vecAux, isc/3, isc%3); // propagator in g0 convention in UKQCD (implicit flavor flip from g5g4)
+      rprop.absorb(vecOut, isc/3, isc%3); // propagator in g0 convention in UKQCD (implicit flavor flip from g5g4)
     }
     rprop.rotateToPhysicalBase_device(-1); // propagator in g0 convention in chiral (flipped flavor sign)
     // compute the prop in momentum space
-    PLEGMA_FT<float> ft((std::vector<float>) {(float)mom[0], (float)mom[1], (float)mom[2], mom[3]+0.5},4,false); // half twist in temporal direction
+    PLEGMA_FT<double> ft((std::vector<double>) {(double)mom[0], (double)mom[1], (double)mom[2], mom[3]+0.5},4,false); // half twist in temporal direction
     //    rprop.scale(1./HGC_totalVolume);
     ft.apply(rprop, FT_GEMV,-1);
     //    rprop.scale(HGC_totalVolume);
@@ -85,14 +81,14 @@ int main(int argc, char **argv)
     site source({0,0,0,0});
     std::vector<GAMMAS> gammas = {ONE,G1,G2,G3,G4,G5,G5G1,G5G2,G5G3,G5G4,S12,S13,S23,S41,S42,S43};     
 
-    PLEGMA_Correlator<float> corr(MOMENTUM_SPACE, source, 0, HGC_totalL[3]);
+    PLEGMA_Correlator<double> corr(MOMENTUM_SPACE, source, 0, HGC_totalL[3]);
     corr.contractNucleonThrp_local(lprop,rprop,0,gammas,true);
     corr.writeFile(filesPrefix+"Vloc_"+pxpypzpt+"_conf_"+confStr,HDF5_FORMAT);
 
-    corr.contractNucleonThrp_oneD(lprop,rprop,fgauge,0,gammas,true);
+    corr.contractNucleonThrp_oneD(lprop,rprop,gauge,0,gammas,true);
     corr.writeFile(filesPrefix+"VoneD_"+pxpypzpt+"_conf_"+confStr,HDF5_FORMAT);
 
-    corr.contractNucleonThrp_twoD(lprop,rprop,fgauge,0,gammas,true);
+    corr.contractNucleonThrp_twoD(lprop,rprop,gauge,0,gammas,true);
     corr.writeFile(filesPrefix+"VtwoD_"+pxpypzpt+"_conf_"+confStr,HDF5_FORMAT);
   }
 
