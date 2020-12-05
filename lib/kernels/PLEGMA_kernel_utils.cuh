f21d788bb7712b3acc35552b3805a72e9f63666c
Simone Bacchio <s.bacchio@gmail.com>
2020-12-05 16:54:53 +0200

    Fixing bug in links product

--- lib/kernels/PLEGMA_kernel_utils.cuh
+++ lib/kernels/PLEGMA_kernel_utils.cuh
@@ -341,6 +341,18 @@ namespace plegma {
     }
   }
 
+  template<bool isDagB, bool isDagC, typename FloatA, typename FloatB, typename FloatC>
+  __inline__ __device__ void mul_G_G(Float2<FloatA> a[N_COLS][N_COLS], Float2<FloatB> b[N_COLS][N_COLS], Float2<FloatC> c[N_COLS][N_COLS]){
+    if(isDagB and isDagC)
+      mul_Gdag_Gdag(a,b,c);
+    else if(isDagB)
+      mul_Gdag_G(a,b,c);
+    else if(isDagC)
+      mul_G_Gdag(a,b,c);
+    else
+      mul_G_G(a,b,c);
+  }
+
   template<bool isLeftTrans, ACCUM_TYPE aty,typename FloatA, typename FloatB, typename FloatC>
   __inline__ __device__ void partial_trace_mul_Prop_Prop(Float2<FloatA> A[N_SPINS][N_SPINS],
 							 Float2<FloatB> B[N_SPINS][N_SPINS][N_COLS][N_COLS],
@@ -477,21 +489,7 @@ namespace plegma {
 							       Float2<FloatD> D2[N_COLS][N_COLS],
 							       int s1=-1,int s2=-1,int c1=-1, int c2=-1){
     Float2<FloatD> D[N_COLS][N_COLS];
-    #pragma unroll
-    for(int a = 0; a < N_COLS; a++)
-      #pragma unroll
-      for(int b = 0; b < N_COLS; b++)
-        #pragma unroll
-	for(int c = 0; c < N_COLS; c++) {
-	  if(isG1dag and not isG2dag)
-	    D[a][c] = conj(D1[b][a])*D2[b][c];
-	  else if(not isG1dag and isG2dag)
-	    D[a][c] = D1[a][b]*conj(D2[c][b]);
-	  else if(isG1dag and isG2dag)
-	    D[a][c] = conj(D1[b][a])*conj(D2[c][b]);
-	  else
-	    D[a][c] = D1[a][b]*D2[b][c];
-	}
+    mul_G_G<isG1dag,isG2dag>(D,D1,D2);
     partial_trace_mul_Prop_G_Prop<isLeftTrans,aty,false>(A,B,C,D,s1,s2,c1,c2);
   }
 
@@ -504,21 +502,7 @@ namespace plegma {
 								  Float2<FloatD> D3[N_COLS][N_COLS],
 								  int s1=-1,int s2=-1,int c1=-1, int c2=-1){
     Float2<FloatD> D[N_COLS][N_COLS];
-    #pragma unroll
-    for(int a = 0; a < N_COLS; a++)
-      #pragma unroll
-      for(int b = 0; b < N_COLS; b++)
-        #pragma unroll
-	for(int c = 0; c < N_COLS; c++) {
-	  if(isG1dag and not isG2dag)
-	    D[a][c] = conj(D1[b][a])*D2[b][c];
-	  else if(not isG1dag and isG2dag)
-	    D[a][c] = D1[a][b]*conj(D2[c][b]);
-	  else if(isG1dag and isG2dag)
-	    D[a][c] = conj(D1[b][a])*conj(D2[c][b]);
-	  else
-	    D[a][c] = D1[a][b]*D2[b][c];
-	}
+    mul_G_G<isG1dag,isG2dag>(D,D1,D2);
     partial_trace_mul_Prop_G1_G2_Prop<isLeftTrans,aty,false,isG3dag>(A,B,C,D,D3,s1,s2,c1,c2);
   }
 
