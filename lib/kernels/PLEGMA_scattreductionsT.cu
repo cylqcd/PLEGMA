a75223c29f64dd678e1231a16cc0211d0c941167
pittlerf <pittler@hiskp.uni-bonn.de>
2021-02-22 22:41:24 +0100

    adding N-N reductions with 1 interpolator

--- lib/kernels/PLEGMA_scattreductionsT.cu
+++ lib/kernels/PLEGMA_scattreductionsT.cu
@@ -30,6 +30,8 @@ void T_kernels_wrapper( ProfileStruct &ps, TRED T, Float2<FloatOut> *block2,
   switch(listGammas_i.size){
   case(1):
     switch(listGammas_f.size){
+    case(1): T_kernels<(unsigned int)1,(unsigned int)1,FloatOut,FloatP>( ps, T, block2, it, time_step, maxT, source, moms, listGammas_i, listGammas_f, S1, S2, S3); break;
+
     case(3): T_kernels<(unsigned int)1,(unsigned int)3,FloatOut,FloatP>( ps, T, block2, it, time_step, maxT, source, moms, listGammas_i, listGammas_f, S1, S2, S3); break;
 //    case(4): T_kernels<(unsigned int)1,(unsigned int)4,FloatOut,FloatP>( ps, T, block2, it, time_step, maxT, source, moms, listGammas_i, listGammas_f, S1, S2, S3); break;
 //    case(9): T_kernels<(unsigned int)1,(unsigned int)9,FloatOut,FloatP>( ps, T, block2, it, time_step, maxT, source, moms, listGammas_i, listGammas_f, S1, S2, S3); break;
