9dc87dc5c85fae3d3921aa681bf3640985cc115c
sy3394 <shuhei.yamamoto.2011@gmail.com>
2021-06-07 20:26:21 +0300

    fixed a minor bug in HDF5 saving in a single-rank mode

--- include/io/PLEGMA_hdf5.h
+++ include/io/PLEGMA_hdf5.h
@@ -123,8 +123,8 @@ protected:
     return product;
   }
   inline hsize_t to_id(std::vector<hsize_t> ids, std::vector<hsize_t> shape){
-    hsize_t id = ids[0];
-    for(size_t i = 1; i < ids.size(); i++) id = id*shape[i] + ids[i];
+    hsize_t id = ids[0]%shape[0];
+    for(size_t i = 1; i < ids.size(); i++) id = id*shape[i] + ids[i]%shape[i];
     return id;
   }
   inline std::vector<hsize_t> from_id(hsize_t id, std::vector<hsize_t> shape){
@@ -331,16 +331,18 @@ protected:
     // In this function only one processor writes
     if(getRank() == 0) {
       bool needs_shift = false;
-      T* tmp = buf;
+      T* tmp;
       if(!start.empty()) for (auto i: start) if(i != 0) needs_shift = true;
       
-      // Shifting the data accordingly to start
+      // Shifting the data accordingly to start in this function
       if(needs_shift) {
 	hostMalloc(tmp, product(shape)*sizeof(T));
 	for(hsize_t i = 0; i<product(shape); i++) {
 	  hsize_t j = to_id( add( from_id(i, shape), start), shape);
-	  tmp[i] = buf[j];
+	  tmp[j] = buf[i];
 	}
+      } else {
+	tmp = buf;
       }
 
       _write_dataset_parallel(dataset_id, tmp, shape, shape, zeros_like(shape), true);
