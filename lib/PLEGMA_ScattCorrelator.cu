524262f479ce725cb63bb7aa94af990839cbc6d9
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2021-06-16 11:40:30 +0300

    include new type of P diagrams

--- lib/PLEGMA_ScattCorrelator.cu
+++ lib/PLEGMA_ScattCorrelator.cu
@@ -582,7 +582,7 @@ void PLEGMA_ScattCorrelator<Float>::initialize_diagram( std::vector<GAMMAS_SCATT
 template<typename Float>
 void PLEGMA_ScattCorrelator<Float>::initialize_diagram( std::vector<GAMMAS_SCATT> &G_i2, std::vector<GAMMAS_SCATT> &G_f2, std::string name_of_diagram){
 
-  assert( name_of_diagram=="P" );
+  assert(( name_of_diagram=="P") ||  (name_of_diagram=="P0DN") ||  (name_of_diagram=="P0UP") ||  (name_of_diagram=="PPDN") ||  (name_of_diagram=="PPUP" ) );
 
   //Gamma list
   this->GList.clear();
