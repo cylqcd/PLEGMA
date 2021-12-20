c9458efe5dbdf5388012190fbcce753fefcc4cbc
khadjiyiannakou <k.hadjiyiannakou@cyi.ac.cy>
2021-12-20 14:22:07 +0200

    This is already in master

--- plegma/Zfac.cpp
+++ plegma/Zfac.cpp
@@ -88,11 +88,11 @@ int main(int argc, char **argv)
     corr.contractNucleonThrp_oneD(lprop,rprop,gauge,0,gammas,true);
     corr.writeFile(filesPrefix+"VoneD_"+pxpypzpt+"_conf_"+confStr,HDF5_FORMAT);
 
-    corr.contractNucleonThrp_twoD(lprop,rprop,gauge,0,gammas,true);
-    corr.writeFile(filesPrefix+"VtwoD_"+pxpypzpt+"_conf_"+confStr,HDF5_FORMAT);
+    // corr.contractNucleonThrp_twoD(lprop,rprop,gauge,0,gammas,true);
+    // corr.writeFile(filesPrefix+"VtwoD_"+pxpypzpt+"_conf_"+confStr,HDF5_FORMAT);
     
-    corr.contractNucleonThrp_threeD(lprop,rprop,gauge,0,gammas,true);
-    corr.writeFile(filesPrefix+"VthreeD_"+pxpypzpt+"_conf_"+confStr,HDF5_FORMAT);
+    // corr.contractNucleonThrp_threeD(lprop,rprop,gauge,0,gammas,true);
+    // corr.writeFile(filesPrefix+"VthreeD_"+pxpypzpt+"_conf_"+confStr,HDF5_FORMAT);
   }
 
     
