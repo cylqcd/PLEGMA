77b66569fccd5adeee2be882d610607ab9621f47
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2022-01-31 08:50:54 +0100

    trying to improve on W diagrams

--- lib/PLEGMA_ScattCorrelator.cu
+++ lib/PLEGMA_ScattCorrelator.cu
@@ -681,65 +681,62 @@ void PLEGMA_ScattCorrelator<Float>::V5V6reduction(PLEGMA_ScattCorrelator<Float> 
 
 
     #pragma omp parallel for
-    for(int i_m=0; i_m<imap.size(); i_m++){
-      Float temp[2*N_SPINS*N_SPINS];
-      int i_mom_f1 = imap[i_m][1];
-      int i_mom_f2 = imap[i_m][2];
-      if ((i_mom_f2==input_mom_f2)){
-        for (int g2=0 ; g2 < n_gammas_i2 ; ++g2 ){//gi2
-          Float phi0Aux[N_SPINS*N_COLS*2];
-          GAMMAS_SCATT gamma5 = G_5;
-          GAMMAS_SCATT gamma5_t_gammai2 = apply_g5( this->GList[3][g2], LEFT);
-          V_MVM<Float>( Phi0.get(), gamma5, gamma5_t_gammai2, phi0Aux );
+    for(int t=0; t < TIME; ++t){
+      int global_time_index = t + HGC_procPosition[3] * HGC_localL[3];
 
-          for(int t=0; t < TIME; ++t){
+      PLEGMA_Vector<Float> tmp(HOST);
+      tmp.copy(*Phi_1[global_time_index],HOST);
 
-	    int global_time_index = t + HGC_procPosition[3] * HGC_localL[3];
+      std::shared_ptr<Float> Phi1 = tmp.getPointSource(actualSource,HOST);
 
-	    PLEGMA_Vector<Float> tmp(HOST);
-	    tmp.copy(*Phi_1[global_time_index],HOST);
+      for (int g2=0 ; g2 < n_gammas_i2 ; ++g2 ){//gi2
+        Float phi0Aux[N_SPINS*N_COLS*2];
+        GAMMAS_SCATT gamma5 = G_5;
+        GAMMAS_SCATT gamma5_t_gammai2 = apply_g5( this->GList[3][g2], LEFT);
+        V_MVM<Float>( Phi0.get(), gamma5, gamma5_t_gammai2, phi0Aux );
 
-            std::shared_ptr<Float> Phi1 = tmp.getPointSource(actualSource,HOST);
+        for (int g4=0 ; g4 < n_gammas_f2 ; ++g4 ){//gf2
 
-            for (int g4=0 ; g4 < n_gammas_f2 ; ++g4 ){//gf2
+          Float phi1Aux[N_SPINS*N_COLS*2];
+          GAMMAS_SCATT gamma5_t_gammaf2 = apply_g5( this->GList[5][g4], LEFT);
+          V_MVM<Float>( Phi1.get(), gamma5, gamma5_t_gammaf2, phi1Aux );
 
-              Float phi1Aux[N_SPINS*N_COLS*2];
-              GAMMAS_SCATT gamma5_t_gammaf2 = apply_g5( this->GList[5][g4], LEFT);
-              V_MVM<Float>( Phi1.get(), gamma5, gamma5_t_gammaf2, phi1Aux );
+          for (int g1=0 ; g1 < n_gammas_i1 ; ++g1 ){//gi1
+            for (int g3=0 ; g3 < n_gammas_f1 ; ++g3 ){//gf1
+              Float V5Aux[NS2C];
+              Float V6Aux[NS1C];
+	      Float V5Aux2[NS1C];
+	      for (int ii=0;ii<NS2C;++ii)
+	        V5Aux[ii]=0;
+              for (int ii=0;ii<NS1C;++ii){
+                V5Aux2[ii]=0;
+	        V6Aux[ii]=0;
+	      }
 
-              for (int g1=0 ; g1 < n_gammas_i1 ; ++g1 ){//gi1
-                for (int g3=0 ; g3 < n_gammas_f1 ; ++g3 ){//gf1
 
-	          Float V5Aux[NS2C];
-		  Float V6Aux[NS1C];
-		  Float V5Aux2[NS1C];
-		  for (int ii=0;ii<NS2C;++ii)
-		    V5Aux[ii]=0;
-                  for (int ii=0;ii<NS1C;++ii){
-                    V5Aux2[ii]=0;
-		    V6Aux[ii]=0;
+	      for (int alfa=0; alfa < N_SPINS; ++alfa ){                
+	        for (int beta=0; beta < N_SPINS; ++beta ){
+
+	          for (unsigned short eps1_nz=0; eps1_nz<6; eps1_nz++ ){
+	            unsigned short m=eps_host[eps1_nz][0];
+		    unsigned short a=eps_host[eps1_nz][1];
+		    unsigned short b=eps_host[eps1_nz][2];
+		    int eps1_sgn=sgn_eps_host[eps1_nz];
+		    Float tmp[2];
+		    tmp[0]= (phi0Aux[2*(alfa*N_COLS+a)]*phi1Aux[2*(beta*N_COLS+b)]-phi0Aux[2*(alfa*N_COLS+a)+1]*phi1Aux[2*(beta*N_COLS+b)+1])*(Float)eps1_sgn;
+		    tmp[1]= (-phi0Aux[2*(alfa*N_COLS+a)+1]*phi1Aux[2*(beta*N_COLS+b)]-phi0Aux[2*(alfa*N_COLS+a)]*phi1Aux[2*(beta*N_COLS+b)+1])*(Float)eps1_sgn;
+		    V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)]   = V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)] + tmp[0];
+		    V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)+1] = V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)+1] + tmp[1];
 		  }
+                }
+	      }
 
-
-		  for (int alfa=0; alfa < N_SPINS; ++alfa ){                
-		    for (int beta=0; beta < N_SPINS; ++beta ){
-
-		      for (unsigned short eps1_nz=0; eps1_nz<6; eps1_nz++ ){
-			unsigned short m=eps_host[eps1_nz][0];
-			unsigned short a=eps_host[eps1_nz][1];
-			unsigned short b=eps_host[eps1_nz][2];
-			int eps1_sgn=sgn_eps_host[eps1_nz];
-			Float tmp[2];
-			tmp[0]= (phi0Aux[2*(alfa*N_COLS+a)]*phi1Aux[2*(beta*N_COLS+b)]-phi0Aux[2*(alfa*N_COLS+a)+1]*phi1Aux[2*(beta*N_COLS+b)+1])*(Float)eps1_sgn;
-			tmp[1]= (-phi0Aux[2*(alfa*N_COLS+a)+1]*phi1Aux[2*(beta*N_COLS+b)]-phi0Aux[2*(alfa*N_COLS+a)]*phi1Aux[2*(beta*N_COLS+b)+1])*(Float)eps1_sgn;
-			V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)]   = V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)] + tmp[0];
-			V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)+1] = V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)+1] + tmp[1];
-		      }
-		    }
-		  }
-
+              for(int i_m=0; i_m<imap.size(); i_m++){
+                Float temp[2*N_SPINS*N_SPINS];
+                int i_mom_f1 = imap[i_m][1];
+                int i_mom_f2 = imap[i_m][2];
+                if ((i_mom_f2==input_mom_f2)){
 		  
-
 		  for (int alfa=0;alfa<N_SPINS;++alfa){
 		    for (int beta=0; beta<N_SPINS;++beta){
 
@@ -753,7 +750,6 @@ void PLEGMA_ScattCorrelator<Float>::V5V6reduction(PLEGMA_ScattCorrelator<Float> 
 			case 0: absorb_fromV56<0,Float>( V5Aux2, V5Aux, beta ); break;
 			case 1: absorb_fromV56<1,Float>( V5Aux2, V5Aux, beta ); break;
 		      }
-
 
 		      //if true multiply by sigma_T(G_f1)
 		      if(transpgamma_f1){
@@ -834,71 +830,57 @@ void PLEGMA_ScattCorrelator<Float>::V5V6reduction_matrix(PLEGMA_ScattCorrelator<
     auto imap = this->pList().index_map();
 
     #pragma omp parallel for
-    for(int i_m=0; i_m<imap.size(); i_m++){
-      Float temp[2*N_SPINS*N_SPINS];
-      int i_mom_f1 = imap[i_m][1];
-      int i_mom_f2 = imap[i_m][2];
-      if ((i_mom_f2==input_mom_f2) ){
-	for (int g2=0 ; g2 < n_gammas_i2 ; ++g2 ){//gi2
+    for (int t=0; t<TIME; ++t){
+      int global_time_index = t + HGC_procPosition[3] * HGC_localL[3];
+      PLEGMA_Vector<Float> tmp(HOST);
+      tmp.copy(*Phi_1[global_time_index],HOST);
 
-          Float phi0Aux[N_SPINS*N_COLS*2];
-          GAMMAS_SCATT gamma5 = G_5;
-          GAMMAS_SCATT gamma5_t_gammai2 = apply_g5( this->GList[3][g2], LEFT);
-          V_MVM<Float>( Phi0.get(), gamma5, gamma5_t_gammai2, phi0Aux );
+      std::shared_ptr<Float> Phi1 = tmp.getPointSource(actualSource,HOST);
 
+      for (int g2=0 ; g2 < n_gammas_i2 ; ++g2 ){//gi2
+        Float phi0Aux[N_SPINS*N_COLS*2];
+        GAMMAS_SCATT gamma5 = G_5;
+        GAMMAS_SCATT gamma5_t_gammai2 = apply_g5( this->GList[3][g2], LEFT);
+        V_MVM<Float>( Phi0.get(), gamma5, gamma5_t_gammai2, phi0Aux );
+        for (int g4=0 ; g4 < n_gammas_f2 ; ++g4 ){//gf2
+          Float phi1Aux[N_SPINS*N_COLS*2];
+          GAMMAS_SCATT gamma5_t_gammaf2 = apply_g5( this->GList[5][g4], LEFT);
+	  V_MVM<Float>( Phi1.get(), gamma5, gamma5_t_gammaf2, phi1Aux );
 
-	  for (int t=0; t<TIME; ++t){
+          for (int g1=0 ; g1 < n_gammas_i1 ; ++g1 ){//gi1i
+	    for (int g3=0 ; g3 < n_gammas_f1 ; ++g3 ){//gf1
 
-            int global_time_index = t + HGC_procPosition[3] * HGC_localL[3];
+              Float V5Aux[2*N_SPINS*N_SPINS*N_COLS];
+              for (int i=0;i<2*N_SPINS*N_SPINS*N_COLS;++i){
+	        V5Aux[i]=0.;
+	      }
+	      Float V5Aux2[2*N_COLS];
+              for (int i=0;i<2*N_COLS;++i){
+                V5Aux2[i]=0.;
+              }
 
-	    PLEGMA_Vector<Float> tmp(HOST);
-            tmp.copy(*Phi_1[global_time_index],HOST);
-
-            std::shared_ptr<Float> Phi1 = tmp.getPointSource(actualSource,HOST);
-
-
-            for (int g4=0 ; g4 < n_gammas_f2 ; ++g4 ){//gf2
-
-	      Float phi1Aux[N_SPINS*N_COLS*2];
-              GAMMAS_SCATT gamma5_t_gammaf2 = apply_g5( this->GList[5][g4], LEFT);
-	      V_MVM<Float>( Phi1.get(), gamma5, gamma5_t_gammaf2, phi1Aux );
-
- 
-              for (int g1=0 ; g1 < n_gammas_i1 ; ++g1 ){//gi1i
-	        for (int g3=0 ; g3 < n_gammas_f1 ; ++g3 ){//gf1
-
-
-	          Float V5Aux[2*N_SPINS*N_SPINS*N_COLS];
-		  for (int i=0;i<2*N_SPINS*N_SPINS*N_COLS;++i){
-		    V5Aux[i]=0.;
-		  }
-		  Float V5Aux2[2*N_COLS];
-                  for (int i=0;i<2*N_COLS;++i){
-                    V5Aux2[i]=0.;
-                  }
-
-		  Float V6Aux[2*N_SPINS*N_SPINS*N_COLS];
-
-		  for (int alfa=0; alfa < N_SPINS; ++alfa ){                
-		    for (int beta=0; beta < N_SPINS; ++beta ){
-
-		      for (unsigned short eps1_nz=0; eps1_nz<6; eps1_nz++ ){
-			unsigned short m=eps_host[eps1_nz][0];
-			unsigned short a=eps_host[eps1_nz][1];
-			unsigned short b=eps_host[eps1_nz][2];
-			int eps1_sgn=sgn_eps_host[eps1_nz];
-			Float tmp[2];
-			tmp[0]= ( phi0Aux[2*(alfa*N_COLS+a)]*phi1Aux[2*(beta*N_COLS+b)]-phi0Aux[2*(alfa*N_COLS+a)+1]*phi1Aux[2*(beta*N_COLS+b)+1])*(Float)eps1_sgn;
-                        tmp[1]= (-phi0Aux[2*(alfa*N_COLS+a)+1]*phi1Aux[2*(beta*N_COLS+b)]-phi0Aux[2*(alfa*N_COLS+a)]*phi1Aux[2*(beta*N_COLS+b)+1])*(Float)eps1_sgn;
-			V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)]   = V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)] + tmp[0];
-                        V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)+1] = V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)+1] + tmp[1];
-		      }
-		    }
-		  }
-
-		  V_TR_MM<Float>( V5Aux, this->GList[2][g1],transpgamma_i1, V5Aux2);
-
-
+	      Float V6Aux[2*N_SPINS*N_SPINS*N_COLS];
+              for (int alfa=0; alfa < N_SPINS; ++alfa ){                
+	        for (int beta=0; beta < N_SPINS; ++beta ){
+                  for (unsigned short eps1_nz=0; eps1_nz<6; eps1_nz++ ){
+	            unsigned short m=eps_host[eps1_nz][0];
+		    unsigned short a=eps_host[eps1_nz][1];
+		    unsigned short b=eps_host[eps1_nz][2];
+		    int eps1_sgn=sgn_eps_host[eps1_nz];
+		    Float tmp[2];
+		    tmp[0]= ( phi0Aux[2*(alfa*N_COLS+a)]*phi1Aux[2*(beta*N_COLS+b)]-phi0Aux[2*(alfa*N_COLS+a)+1]*phi1Aux[2*(beta*N_COLS+b)+1])*(Float)eps1_sgn;
+                    tmp[1]= (-phi0Aux[2*(alfa*N_COLS+a)+1]*phi1Aux[2*(beta*N_COLS+b)]-phi0Aux[2*(alfa*N_COLS+a)]*phi1Aux[2*(beta*N_COLS+b)+1])*(Float)eps1_sgn;
+		    V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)]   = V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)] + tmp[0];
+                    V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)+1] = V5Aux[2*((alfa*N_SPINS+beta)*N_COLS+m)+1] + tmp[1];
+	          }
+	        }
+	      }
+              V_TR_MM<Float>( V5Aux, this->GList[2][g1],transpgamma_i1, V5Aux2);
+              for(int i_m=0; i_m<imap.size(); i_m++){
+                Float temp[2*N_SPINS*N_SPINS];
+                int i_mom_f1 = imap[i_m][1];
+                int i_mom_f2 = imap[i_m][2];
+                if ((i_mom_f2==input_mom_f2) ){
                   for (int alfa=0;alfa<N_SPINS;++alfa){
                     for (int beta=0;beta<N_SPINS;++beta){
 		      for (int coloridx=0;coloridx<N_COLS;++coloridx){
@@ -910,12 +892,10 @@ void PLEGMA_ScattCorrelator<Float>::V5V6reduction_matrix(PLEGMA_ScattCorrelator<
 
                   //if true multiply by sigma_T(G_f1)
 
-
                   if(transpgamma_f1){
                     for(int sc=0; sc<N_SPINS*N_SPINS*N_COLS*2; ++sc)
                       V6Aux[sc] *= gammaTranspSign_scatt[this->GList[4][g3]];
                   }
-
 
 		  for (int alfa=0;alfa<N_SPINS;++alfa){
 		    for (int beta=0; beta<N_SPINS;++beta){
@@ -953,13 +933,13 @@ void PLEGMA_ScattCorrelator<Float>::V5V6reduction_matrix(PLEGMA_ScattCorrelator<
                                 egammaf, egammai, temp );
 		    }//Gextf
                   }//Gexti
-                }//Gf1
-              }//Gi1
-            }//Gf2
-          }//Gi12
-        }//t
-      }//if mom
-    }//mom
+                }//if mom
+              }//imom
+            }//Gf1
+          }//Gi1 
+        }//Gf2 
+      }//Gi2
+    }//t
 }
 
 
