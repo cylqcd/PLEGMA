18c2b7f62aeb917a9a3562e6ccd9f1b5d3a84b58
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2022-09-27 11:35:19 +0200

    changing PLEGMA_utils

--- utils/PLEGMA_utils.cpp
+++ utils/PLEGMA_utils.cpp
@@ -72,13 +72,10 @@ void updateOptions(WHICHFLAVOR fl){
   return updateOptions(filename, aux);
 }
 
-void initializePLEGMA(int device_num) {
+void initializePLEGMA() {
   if(!isInitOpt){fprintf(stderr,"initializeOptions should be called before initializePLEGMA");exit(EXIT_FAILURE);}
   HGC_options->close();
-  if (device_num==-2)
-    initQuda(device_number);
-  else
-    initQuda(device_num);
+  initQuda(device_number);
   qudaInitialized=true;
   // initialize PLEGMA params
   PLEGMA_init(dims, procs, verbosity);
