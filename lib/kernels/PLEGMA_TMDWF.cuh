3cfcb50de24a64b52f358ab4c85b7320a2bdca94
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2022-10-04 10:52:50 +0200

    correcting time_step

--- lib/kernels/PLEGMA_TMDWF.cuh
+++ lib/kernels/PLEGMA_TMDWF.cuh
@@ -78,7 +78,7 @@ void contract_TMDWF_mesons_host( ProfileStruct &ps,
 
   int t_size = corr.localT(); if(t_size==0) return;
   int maxT = corr.endT() - corr.startT(); 
-  int time_step = ps.tp.grid.x*ps.tp.block.x/HGC_localVolume3D;
+  int time_step =  get_time_step(ps.tp.grid.x, ps.tp.block.x);
   bool runFT = (corr.getCorrSpace()==MOMENTUM_SPACE);
   size_t size = corr.getTotalSize()/t_size*time_step;
   size_t volume = corr.getVolSize()/t_size;
