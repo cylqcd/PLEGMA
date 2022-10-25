6772da0cdf667db1f7ff596d3f0a710aa5dd6364
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2022-10-25 21:01:42 +0200

    using device::get_stream instead of target::cuda::get_stream

--- lib/kernels/PLEGMA_kernel_tuner.cuh
+++ lib/kernels/PLEGMA_kernel_tuner.cuh
@@ -274,7 +274,7 @@ void PLEGMA_kernel_tuner<types...>::run(){
   launchKernel(ps.tp.grid,ps.tp.block,ps.tp.shared_bytes,0);
 #else
   if(!ps.tuned) ps.tp = tuneLaunch(*this, QUDA_TUNE_NO, (QudaVerbosity) HGC_verbosity);
-  launchKernel(ps.tp,target::cuda::get_stream(0));
+  launchKernel(ps.tp,device::get_stream(0));
 #endif
   checkQudaError();
 }
