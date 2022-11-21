2c6edb84ddba91f9ead5ccc1cf9a94959bb81992
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2022-11-21 18:21:17 +0100

    continue hipify

--- lib/kernels/PLEGMA_threep_threeD_part3.cu
+++ lib/kernels/PLEGMA_threep_threeD_part3.cu
@@ -6,6 +6,7 @@
 #include <PLEGMA_threep.cuh>
 #include <PLEGMA_Vector.h>
 #include <malloc_quda.h>
+#include <quda_api.h>
 using namespace plegma;
 template<typename T>
 struct KernelArr {T* array; int size;};
@@ -167,7 +168,7 @@ static void threep_threeD_part3_host(ProfileStruct &ps, Float2<FloatC> *result, 
 //  cudaMalloc((void**)&listGammas.array, gammas.size()*sizeof(GAMMAS));
   listGammas.array=(GAMMAS*)device_malloc(gammas.size()*sizeof(GAMMAS));
 
-  cudaMemcpy(listGammas.array, gammas.data(), gammas.size()*sizeof(GAMMAS), cudaMemcpyHostToDevice);
+  qudaMemcpy(listGammas.array, gammas.data(), gammas.size()*sizeof(GAMMAS), qudaMemcpyHostToDevice);
 
   if(HGC_verbosity > 2)
     if(corr.hasSource())
@@ -220,7 +221,7 @@ static void threep_threeD_part3_host(ProfileStruct &ps, Float2<FloatC> *result, 
 	 source, signProps, runFT, *moms, dir1,dir2,dir3,mu,nu,c1,c2);
       error=cudaPeekAtLastError(); if(error != cudaSuccess) goto exit;
       
-      cudaMemcpy(h_partial_block, d_partial_block, (alloc_size/time_step)*t_step*sizeof(Float2<FloatC>), cudaMemcpyDeviceToHost);
+      qudaMemcpy(h_partial_block, d_partial_block, (alloc_size/time_step)*t_step*sizeof(Float2<FloatC>), qudaMemcpyDeviceToHost);
       error=cudaPeekAtLastError(); if(error != cudaSuccess) goto exit;
       
       if(runFT==true){
@@ -242,8 +243,8 @@ static void threep_threeD_part3_host(ProfileStruct &ps, Float2<FloatC> *result, 
   
  exit:
   hostFree(h_partial_block, alloc_size*sizeof(FloatC));
-  cudaFree(d_partial_block);
-  cudaFree(listGammas.array);
+  device_free(d_partial_block);
+  device_free(listGammas.array);
 }
 
 template<bool b,typename FloatC,typename FloatA,typename FloatB,typename FloatG>
