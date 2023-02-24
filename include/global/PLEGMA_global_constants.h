7b319176a550aae53d2d73d1976e95271a7c4db9
Ferenc Pittler <pittler@hiskp.ubi-bonn.de>
2023-02-24 14:42:23 +0200

    Constant for HIP

--- include/global/PLEGMA_global_constants.h
+++ include/global/PLEGMA_global_constants.h
@@ -20,26 +20,32 @@
 #endif
 #ifdef ADD_TO_GLOBAL
 
+void *symbolAddress;
 #define global_host(dtype, name, ...)					\
   HGC_global_vars.add<dtype>(#name,					\
 				   PRODUCT(__VA_ARGS__),		\
 				   &HGC_##name PARENTHESES(0,__VA_ARGS__))
 
 #define global_both(dtype, name, ...)					\
+  HIP_CHECK(hipGetSymbolAddress(&symbolAddress, HIP_SYMBOL(DGC_##name))); \
   HGC_global_vars.add<dtype>(#name,					\
 				   PRODUCT(__VA_ARGS__),		\
 				   &HGC_##name PARENTHESES(0,__VA_ARGS__), \
-			           (void**) &DGC_##name)
+			           (void**) &symbolAddress)
 
 #else
 #ifdef ALLOCATE
 
 #define global_host(dtype, name, ...)					\
   dtype HGC_##name PARENTHESES(1,__VA_ARGS__);
-#if defined (  __HIP__ ) || (  __NVCC__ ) 
+#if defined (  __NVCC__ ) 
 #define global_both(dtype, name, ...)					\
   dtype HGC_##name PARENTHESES(1,__VA_ARGS__);				\
-  __constant__ dtype DGC_##name PARENTHESES(1,__VA_ARGS__);		
+  __constant__ dtype DGC_##name PARENTHESES(1,__VA_ARGS__);	
+#elif defined (__HIP__) 
+#define global_both(dtype, name, ...)                                   \
+  dtype HGC_##name PARENTHESES(1,__VA_ARGS__);                          \
+  __device__ __constant__ dtype DGC_##name PARENTHESES(1,__VA_ARGS__);     
 #else
 #define global_both(dtype, name, ...)					\
   dtype HGC_##name PARENTHESES(1,__VA_ARGS__);				
@@ -49,10 +55,14 @@
 
 #define global_host(dtype, name, ...)					\
   extern dtype HGC_##name PARENTHESES(1,__VA_ARGS__);
-#if defined (  __HIP__ ) ||(  __NVCC__ ) 
+#if defined (  __NVCC__ ) 
 #define global_both(dtype, name, ...)					\
   extern dtype HGC_##name PARENTHESES(1,__VA_ARGS__);			\
   extern __constant__ dtype DGC_##name PARENTHESES(1,__VA_ARGS__); 
+#elif defined ( __HIP__ )
+#define global_both(dtype, name, ...)                                   \
+  extern dtype HGC_##name PARENTHESES(1,__VA_ARGS__);                   \
+  extern __device__ __constant__ dtype DGC_##name PARENTHESES(1,__VA_ARGS__);
 #else
 #define global_both(dtype, name, ...)			\
   extern dtype HGC_##name PARENTHESES(1,__VA_ARGS__);
@@ -72,9 +82,6 @@ global_host(Options *, options);
 global_host(bool, hold_exit);
 
 // variables visible on both host and device
-global_both(size_t, localVolume);
-global_both(size_t, localVolume3D);
-global_both(size_t, totalVolume);
 global_both(int, localL, N_DIMS);
 global_both(int, totalL, N_DIMS);
 global_both(int, procPosition, N_DIMS);
@@ -90,6 +97,9 @@ global_both(size_t, vertexGhostVolume3D);
 global_both(size_t, surface3D, N_DIMS);
 global_both(size_t, surface2D, (N_DIMS*(N_DIMS-1))/2);
 global_both(size_t, surface1D, (N_DIMS*(N_DIMS-1)*(N_DIMS-2))/6);
+global_both(size_t, localVolume);
+global_both(size_t, localVolume3D);
+global_both(size_t, totalVolume);
 
 // for mpi use global variables (host only)
 global_both(bool, dimBreak, N_DIMS);
--- include/global/PLEGMA_structs.h
+++ include/global/PLEGMA_structs.h
@@ -1,4 +1,12 @@
 //======== Some custom data struct =========//
+#define HIP_CHECK(error)                                                                           \
+  {                                                                                                \
+    hipError_t localError = error;                                                                 \
+    if ((localError != hipSuccess) && (localError != hipErrorPeerAccessAlreadyEnabled)) {          \
+      PLEGMA_printf("Error: %s, Code %d\n", hipGetErrorString(localError), localError);            \
+      PLEGMA_printf("FILE: %s, LINE %d\n", __FILE__, __LINE__ );			           \
+    }												   \
+  }
 template<typename Float> struct texture;
 
 struct site : std::array<int,N_DIMS> {
@@ -73,18 +81,26 @@ struct pointer_holder {
 
   void copyToDeviceConstant() {
     if(devPointer != nullptr) {
-      //cudaError_t err = qudaMemcpyToSymbol( *devPointer, hostPointer, bytes*size);
-      //if (err != cudaSuccess) {
+#ifdef __NVCC__ 
+    cudaError_t err = cudaMemcpyToSymbol( *devPointer, hostPointer, bytes*size);
+    if (err != cudaSuccess) {
         errorQuda("Failed to copy constant host memory of size to device %zu \n", size);
-      //}
+    }
+#else
+    HIP_CHECK(hipMemcpy(*devPointer, hostPointer, bytes*size, hipMemcpyHostToDevice));
+#endif
     }
   }
   void copyFromDeviceConstant() {
     if(false and devPointer != nullptr) {
-      //cudaError_t err = qudaMemcpyFromSymbol(hostPointer, *devPointer, bytes*size, 0, cudaMemcpyDeviceToHost);
-      //if (err != cudaSuccess) {
+#ifdef __NVCC__
+      cudaError_t err = cudaMemcpyFromSymbol(hostPointer, *devPointer, bytes*size, 0, cudaMemcpyDeviceToHost);
+      if (err != cudaSuccess) {
         errorQuda("Failed to copy constant host memory of size to device %zu \n", size);
-      //}
+      }
+#else
+     qudaMemcpy(hostPointer, *devPointer,  bytes*size,qudaMemcpyDeviceToHost);
+#endif
     }
   }
   bool checkDeviceConstant() {
