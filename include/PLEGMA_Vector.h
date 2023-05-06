606b9f3088f87c4bc79efa3fe1b4240675f9d885
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2023-05-06 13:58:22 +0200

    adding etmc basis change function

--- include/PLEGMA_Vector.h
+++ include/PLEGMA_Vector.h
@@ -93,6 +93,7 @@ namespace plegma {
        @brief Performs the similarity transformation of gamma matrices from tmLQCD to QUDA-UKQCD and vice versa
      **/
     void rotate_uk_ch_g5g4();
+    void rotate_uk_ch_etmc();
     void covD(PLEGMA_Vector<Float> &vecIn, PLEGMA_Gauge<Float> &gauge, int dirOr);
     void mulGV(PLEGMA_Vector<Float> &vecIn, PLEGMA_Su3field<Float> &u);
     void pack_fermion_to_sink(std::vector<PLEGMA_Vector<Float>*> &stochastic_vector, int sinktime);
