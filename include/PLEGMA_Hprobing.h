54641628eac02797fb5b5e433f489b14fe64fbb2
Ferenc Pittler <pittler@hiskp.ubi-bonn.de>
2023-06-21 10:32:47 +0300

    avoid warning

--- include/PLEGMA_Hprobing.h
+++ include/PLEGMA_Hprobing.h
@@ -97,7 +97,7 @@ namespace plegma {
 	arrlc = new int[Nc];
       }
       catch (const std::bad_alloc& err) {
-	PLEGMA_error(err.what());
+	PLEGMA_error("%s\n",err.what());
       }
       createElemColBlock();
       createColLattice();
